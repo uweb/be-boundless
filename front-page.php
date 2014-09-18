5edc359a89eeef681c60198e412a29909f2e2ae5
Dane Odekirk <dizzzy@uw.edu>
2014-09-18 08:16:34 -0700

    Removing loop from homepage.

--- front-page.php
+++ front-page.php
@@ -1,10 +1,6 @@
 <?php get_header(); ?>
 
-
 		<?php
-			// Start the Loop.
-			while ( have_posts() ) : the_post();
-
 				/*
 				 * Include the post format-specific template for the content. If you want to
 				 * use this in a child theme, then include a file called called content-___.php
@@ -15,14 +11,6 @@
 				get_template_part( 'content', 'links' );
 
 				get_template_part( 'content', 'charts' );
-
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) {
-					comments_template();
-				}
-
-			endwhile;
 		?>
-
 
 <?php get_footer(); ?>
