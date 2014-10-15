689c13d40c213bdcf7681fca3b95a1b1f0d82f85
Jon Swanson <jswan2@uw.edu>
2014-10-15 12:51:02 -0700

    added $UW extensions instead of $UW overrides for scripts and images

--- setup/uw.homepage-scripts.php
+++ setup/uw.homepage-scripts.php
@@ -1,27 +1,35 @@
 <?php
 
-class Homepage_Scripts extends UW_Scripts
-{
+//class Homepage_Scripts extends UW_Scripts
+//{
+//
+//  function __construct()
+//  {
+//
+//        $this->SCRIPTS = array(
+//
+//          'homepage' => array (
+//            'id'      => 'homepage',
+//            'url'     => get_bloginfo( 'stylesheet_directory' ) . '/js/homepage'. $this->dev_script() .'.js',
+//            'deps'    => array( 'backbone' ),
+//            'version' => '1.0',
+//            'admin'   => false,
+//          ),
+//
+//      );
+//
+//      parent::__construct();
+//
+//  }
+//
+//}
+//
+//$UW->Scripts = new Homepage_Scripts();
 
-  function __construct()
-  {
-
-        $this->SCRIPTS = array(
-
-          'homepage' => array (
-            'id'      => 'homepage',
-            'url'     => get_bloginfo( 'stylesheet_directory' ) . '/js/homepage'. $this->dev_script() .'.js',
-            'deps'    => array( 'backbone' ),
-            'version' => '1.0',
-            'admin'   => false,
-          ),
-
-      );
-
-      parent::__construct();
-
-  }
-
-}
-
-new Homepage_Scripts;
+$UW->Scripts->SCRIPTS['homepage'] = array (
+    'id'      => 'homepage',
+    'url'     => get_bloginfo( 'stylesheet_directory' ) . '/js/homepage'. $UW->Scripts->dev_script() .'.js',
+    'deps'    => array( 'backbone' ),
+    'version' => '1.0',
+    'admin'   => false,
+  );
