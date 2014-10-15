0f36da416be43fda0063b6e070dc693e1e7f1e00
Jon Swanson <jswan2@uw.edu>
2014-10-15 12:51:52 -0700

    tied into the hook in parent theme

--- functions.php
+++ functions.php
@@ -1,8 +1,6 @@
 <?php
 
-
-add_action( 'init', function() {
-
+add_action( 'extend_uw_object', function($UW) {
   require( 'setup/uw.homepage-scripts.php' );
-
+  require( 'setup/uw.homepage-images.php' );
 });
