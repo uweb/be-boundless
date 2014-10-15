4bc217ba9d475e3b07d13039987042bd96fadd7a
Jon Swanson <jswan2@uw.edu>
2014-10-15 13:57:03 -0700

    overwrote the uw-style (child style) in $UW->Styles with a dev-enabled style

--- functions.php
+++ functions.php
@@ -2,5 +2,6 @@
 
 add_action( 'extend_uw_object', function($UW) {
   require( 'setup/uw.homepage-scripts.php' );
+  require( 'setup/uw.homepage-styles.php' );
   require( 'setup/uw.homepage-images.php' );
 });
