1594da6b42820b6018ae178cbb87baf9ae017859
Dane Odekirk <dizzzy@uw.edu>
2014-10-17 15:53:39 -0700

    Adding mobile image to slideshow.

--- content-slideshow.php
+++ content-slideshow.php
@@ -3,6 +3,11 @@
   <?php if ( class_exists('UW_Slideshow') ) : foreach ( UW_Slideshow::get_latest_slideshow() as $slide ) : ?>
 
   <div class="uw-hero-image uw-homepage-slider slide-<?php echo $slide->id ?>" data-id="<?php echo $slide->id; ?>" style="background:url(<?php echo $slide->image; ?>) no-repeat center; background-size:cover;">
+
+  <?php if ( isset( $slide->mobileimage ) ) : ?>
+    <img src="<?php echo $slide->mobileimage; ?>" alt="<?php esc_attr( $slide->mobileimage ); ?>" class="mobile-image" />
+  <?php endif; ?>
+
     <div class="container hero-container vertical-center">
 
       <div class="row">
@@ -17,7 +22,6 @@
         </div>
 
       </div>
-
 
     </div>
 
