0da284c5f7f35217c41dfc7a13f870bb8f506527
Kilian <kilianf@uw.edu>
2014-10-17 11:28:54 -0700

    Homepage style updates

--- setup/uw.homepage-images.php
+++ setup/uw.homepage-images.php
@@ -3,7 +3,7 @@
 $UW->Images->IMAGE_SIZES['homepage_tile'] =
     array(
       'name' 	=> 'Homepage Tile',
-      'width'	=> 380,
+      'width'	=> 379,
       'height'  => 160,
       'crop'	=> true,
       'show'	=> false
