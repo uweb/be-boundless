9770686d981c043683871b1a49a354bf87a080d4
Dane Odekirk <dizzzy@uw.edu>
2014-10-17 17:33:05 -0700

    Removing mobile image from live site.

--- content-slideshow.php
+++ content-slideshow.php
@@ -4,10 +4,6 @@
 
   <div class="uw-hero-image uw-homepage-slider slide-<?php echo $slide->id ?>" data-id="<?php echo $slide->id; ?>" style="background:url(<?php echo $slide->image; ?>) no-repeat center; background-size:cover;">
 
-  <?php if ( isset( $slide->mobileimage ) ) : ?>
-    <img src="<?php echo $slide->mobileimage; ?>" alt="<?php esc_attr( $slide->mobileimage ); ?>" class="mobile-image" />
-  <?php endif; ?>
-
     <div class="container hero-container vertical-center">
 
       <div class="row">
