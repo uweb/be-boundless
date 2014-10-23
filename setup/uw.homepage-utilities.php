3233868990a00a2ea55d9e3968c187467f32d52e
Jon Swanson <jswan2@uw.edu>
2014-10-23 16:31:03 -0700

    keeping structure but reverting function

--- setup/uw.homepage-utilities.php
+++ setup/uw.homepage-utilities.php
@@ -3,8 +3,10 @@
 class HomepageUtilities {
 
     public function agnostify_url($url){
-        $url = str_replace('http://', '//', $url);
-        return str_replace('https://', '//', $url);
+        //$url = str_replace('http://', '//', $url);
+        //return str_replace('https://', '//', $url);
+        //pubcookie is causing issues here
+        return $url;
     }
 
 }
