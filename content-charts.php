e44fca7ccf186124057842c40283523a1ddcda61
Kilian <kilianf@uw.edu>
2014-11-04 14:19:13 -0800

    New graphics

--- content-charts.php
+++ content-charts.php
@@ -4,72 +4,72 @@
     <div class="row">
 
       <div class="uw-fast-facts">
-        <span>Fast Facts</span>
+        <span>Fact Facts</span>
       </div>
 
-      <div class="fact-1 col-md-5 col-md-offset-1">
+      <div class="fact-1 col-md-6 campus-life">
+        <h4>Campus Life</h4>
+        <div class="campus-life-facts">
 
-          <div><span>#</span><div id="rank">1</div></div>
-          <p class="fact-text">The UW is top-ranked school in medical education for family, rural and primary care</p>
+          <div class="fact-1">
+            <p>14</p>
+            <span>Libraries</span>
+          </div>
 
+          <div class="fact-2">
+            <p>4</p>
+            <span>Markets</span>
+          </div>
+
+          <div class="fact-3">
+            <p>3</p>
+            <span>Food Trucks</span>
+          </div>
+
+          <div class="fact-4">
+            <p>16</p>
+            <span>Coffee shops</span>
+          </div>
+
+          <div class="fact-5">
+            <p>11</p>
+            <span>Residence Halls</span>
+          </div>
+
+          <div class="fact-6">
+            <p>15</p>
+            <span>Restaurants</span>
+          </div>
+
+        </div>
       </div>
 
 
       <div class="col-md-6 faculty-facts">
-        <h4>Faculty Facts</h4>
+        <h4>Faculty Membership</h4>
+
         <div class="row">
-          <div class="col-md-5 title">MacArthur Fellows</div>
-          <div class="col-md-7 people">
-            <div class="person"></div>
-            <div class="person"></div>
-            <div class="person"></div>
-            <div class="person"></div>
-            <div class="person"></div>
-            <div class="person"></div>
-            <div class="person"></div>
-            <div class="person"></div>
-            <div class="person"></div>
-            <div class="person"></div>
-            <div class="person"></div>
-            <div class="person"></div>
-            <div class="person"></div>
-            <div class="person"></div>
-            <div class="person"></div>
-            <div class="person bar"></div>
-            <div class="number">15</div>
+          <div class="people">
+            
+            <div class="fact-1">
+              <p>60</p>
+              <span>Institute of Medicine</span>
+            </div>
+
+            <div class="fact-2">
+              <p>78</p>
+              <span>National Academy of Sciences</span>
+            </div>
+
+            <div class="fact-3">
+              <p>30</p>
+              <span>National Academy of Engineering</span>
+            </div>
+      
+
           </div>
         </div>
-        <div class="row">
-          <div class="col-md-5 title">National Book Award</div>
-          <div class="col-md-7 people">
-            <div class="person"></div>
-            <div class="person"></div>
-            <div class="person bar"></div>
-            <div class="number">2</div>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-md-5 title">Nobel Prize</div>
-          <div class="col-md-7 people">
-            <div class="person"></div>
-            <div class="person"></div>
-            <div class="person"></div>
-            <div class="person"></div>
-            <div class="person"></div>
-            <div class="person"></div>
-            <div class="person bar"></div>
-            <div class="number">6</div>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-md-5 title">Pulitzer Prize</div>
-          <div class="col-md-7 people">
-            <div class="person"></div>
-            <div class="person"></div>
-            <div class="person bar"></div>
-            <div class="number">2</div>
-          </div>
-        </div>
+        
       </div>
 
   </div>
