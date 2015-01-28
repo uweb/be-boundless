4159d5462a76b4fede6f902b824098c2ba2b217d
Dane Odekirk <dizzzy@uw.edu>
2015-01-28 10:47:54 -0800

    Initial commit.

---
+++ gruntfile.js
@@ -0,0 +1,97 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    concat: {
+      options: {
+        separator: ';',
+      },
+      dist: {
+        libraries: [
+        ],
+        theme : [
+        ],
+        src: [ 'js/uw.intro.js', '<%= concat.dist.libraries %>', '<%= concat.dist.theme %>', 'js/uw.outro.js' ],
+        dest: 'js/site.dev.js'
+      }
+    },
+    uglify: {
+      options: {
+        // banner: '/*! <%= pkg.name %> <%= grunt.template.today() %> */\n'
+      },
+      dist: {
+        files: {
+          'js/boundless.js': ['<%= concat.dist.dest %>']
+        }
+      }
+    },
+    jshint: {
+      files: [ 'gruntfile.js', '<%= concat.dist.theme %>' ],
+      options: {
+        asi: true,
+        smarttabs: true,
+        laxcomma: true,
+        lastsemic: true,
+        // options here to override JSHint defaults
+        globals: {
+          jQuery: true,
+          console: true,
+          module: true,
+          document: true
+        }
+      }
+    },
+    notify: {
+      watch: {
+        options: {
+          title: 'Done',
+          message: 'All is good'
+        }
+      }
+    },
+    less: {
+        production: {
+	        options: {
+		        cleancss: true
+			},
+			files: {
+				'style.css': 'less/style.less'
+			}
+		},
+		development: {
+			files: {
+				'style.dev.css': 'less/style.less'
+			}
+		}
+    },
+    watch: {
+      config : {
+        files : ['gruntfile.js'],
+        options : {
+          reload: true
+        }
+      },
+      js: {
+        files: ['<%= concat.dist.src %>'],
+        tasks: ['js']
+      },
+      css: {
+        files: ['less/*.less'],
+        tasks: ['css']
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-notify');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+
+  grunt.registerTask( 'default', ['jshint', 'concat', 'uglify', 'notify', 'less']);
+  grunt.registerTask( 'js', ['jshint', 'concat', 'uglify', 'notify' ]);
+  grunt.registerTask( 'css', ['less', 'notify'] );
+
+};
