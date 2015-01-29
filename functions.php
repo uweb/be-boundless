98333bad0d251ec30f601b476c90bada1972c755
Jon Swanson <jswan2@uw.edu>
2015-01-28 17:16:55 -0800

    rollback functions.php to accomodate the OO-style enqueue

--- functions.php
+++ functions.php
@@ -5,12 +5,4 @@ require('setup/class.boundless.php');
 require 'setup/class.boundless-custom-post.php';
 require 'setup/class.boundless-video-post.php';
 require 'setup/custom-post-types.php';
-
 require( 'setup/class.post-type-map-points.php' );
-
-function add_boundless_sources() {
-  wp_enqueue_style('style', get_stylesheet_uri() . 'style.css');
-  wp_enqueue_script('boundless', get_template_directory_uri() . '/js/boundless.js', array('backbone'), '1.0');
-}
-
-add_action('enqueue_script', 'add_boundless_sources');
