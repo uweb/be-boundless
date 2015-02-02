dd1bb48d70c7c6083746c13d03fd90cfc1b1404f
Jon Swanson <jswan2@uw.edu>
2015-02-02 09:55:14 -0800

    migrated js to sourcemap debugging

--- setup/class.boundless.php
+++ setup/class.boundless.php
@@ -23,7 +23,7 @@ class Boundless
   function scripts()
   {
     wp_register_script( 'google-maps', 'https://maps.googleapis.com/maps/api/js?libraries=places' );
-    wp_register_script( 'boundless', get_template_directory_uri() . '/js/boundless.dev.js', array( 'backbone', 'google-maps' ), self::VERSION );
+    wp_register_script( 'boundless', get_template_directory_uri() . '/js/boundless.js', array( 'backbone', 'google-maps' ), self::VERSION );
     wp_enqueue_script( 'boundless' );
   }
 
