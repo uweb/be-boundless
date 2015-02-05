f1aff56dff7eec009aafbebd9017eff73e08297d
Jon Swanson <jswan2@uw.edu>
2015-02-05 12:20:01 -0800

    condensed the youtube player stuff into a single view

--- js/boundless.video.js
+++ js/boundless.video.js
@@ -26,12 +26,12 @@ BOUNDLESS.Videos = Backbone.Collection.extend({
 
 BOUNDLESS.Video.View = Backbone.View.extend({
 
-  template : '<div class="fullscreen" style="background-image:url(<%= image %>)"><h2 class="video-title"><%= title %></h2><button class="play"></button><div class="behind boundless-youtube" id="video<%= video %>"></div><div class="blurb"><%= text %></div></div>',
+  template : '<div class="fullscreen" style="background-image:url(<%= image %>)"><h2 class="video-title"><%= title %></h2><button class="play"></button><div class="behind boundless-youtube" id="video<%= video %>" aria-label="Video: <%= title %>"></div><div class="blurb"><%= text %></div></div>',
 
   el : '#message',
 
   initialize : function (options) {
-    _.bindAll(this, 'render', 'data_prep');
+    _.bindAll(this, 'render', 'data_prep', 'onReady', 'onStateChange', 'resized', 'youtube_iframe');
     //this is the instantiated collection
     this.collection = BOUNDLESS.videos;
     this.videoNum = parseInt(options.videoNum);
@@ -42,6 +42,7 @@ BOUNDLESS.Video.View = Backbone.View.extend({
     else {
       this.collection.view_to_render = this;
     }
+    this.player_ready = false;
   },
 
   data_prep: function () {
@@ -51,13 +52,73 @@ BOUNDLESS.Video.View = Backbone.View.extend({
     //  return video.get('slug') == this.slug;
     //}
     this.render();
-    this.youtube = new BOUNDLESS.YouTube({youtube_id: this.model.get('video'), $el: this.$el.find('#video' + this.model.get('video')), modest: true});
+    if (BOUNDLESS.youtube_api_ready){
+      this.youtube_iframe();
+    }
+    else {
+      window.addEventListener('youtube_api_ready', this.youtube_iframe);
+    }
   },
 
   render : function () {
     var data = this.model.toJSON();
     var template = _.template(this.template, data);
     this.$el.html(template);
-  }
+    this.$button = this.$el.find('button.play')
+  },
 
+  youtube_iframe : function () {
+    var player_vars = {};
+    if (this.modest) {
+      player_vars = {
+        'rel'           : 0,
+        'controls'      : 0,
+        'modestbranding': 1,
+      }
+    }
+    // if (collection.resolution !== 'undefined'){
+    //     player_vars.VQ = collection.resolution;
+    // }
+    //attach the YT.player to the relevant view, each view gets one
+    this.uwplayer = new YT.Player('video' + this.model.get('video'), {
+      videoId: this.model.get('video'),
+      playerVars: player_vars,
+      events: {
+        //these events will call functions in the view
+       'onReady': this.onReady,
+       'onStateChange': this.onStateChange
+      }
+    });
+  },
+
+  // a resize handler for playlists. Handles the edge case of when a container
+  // is resized to be too small for the list and therefor requires a scrollbar
+  // when none is present or vice versa
+  resized: function() {
+    var viewport_new_width = this.$vidSmall.find('.viewport').width();
+    if (viewport_new_width != this.$viewport_width){
+      this.$viewport_width = viewport_new_width;
+    }
+  },
+
+  onReady: function () {
+    this.play(this.model.get('video'));
+  },
+
+  onStateChange: function (event) {
+    //check for stop playing events
+  },
+
+  // play the video id passed. If 'playnow' not passed, assume false.
+  // If 'playnow' is true play the video, otherwise just cue it up
+  play: function (id, playnow){
+    playnow = playnow || false;
+    if (playnow) {
+      this.uwplayer.loadVideoById(id);
+      this.$el.focus();
+    }
+    else {
+      this.uwplayer.cueVideoById(id);
+    }
+  }
 });
