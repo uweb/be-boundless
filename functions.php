532e681784371986fdf210b6f4ec32558e5701df
Jon Swanson <jswan2@uw.edu>
2015-02-20 10:24:04 -0800

    Removed the homepage utilities and any logic that depends on it

--- functions.php
+++ functions.php
@@ -1,7 +1,6 @@
 <?php
 
 add_action( 'extend_uw_object', function($UW) {
-  require( 'setup/uw.homepage-utilities.php' );
   require( 'setup/uw.homepage-scripts.php' );
   require( 'setup/uw.homepage-styles.php' );
   require( 'setup/uw.homepage-images.php' );
