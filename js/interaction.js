ce1e98de240a4e4d4ce452431777ea1d89566b63
Jon Swanson <jswan2@uw.edu>
2015-03-06 13:51:11 -0800

    switched from animate to css3 translate

--- js/interaction.js
+++ js/interaction.js
@@ -9,16 +9,18 @@ BOUNDLESS.Navigation = Backbone.View.extend({
 
   events : {
     'click li' : 'segueOut',
-    'click .show-nav' : 'segueIn',
+    'click .show-nav' : 'segueIn'
   },
 
   initialize : function( options )
   {
     _.bindAll( this,
+     'transitionDone',
      'bounce',
      'resetMargins'
      )
     this.$toggle = this.$('.show-nav')
+    this.$el.on(BOUNDLESS.TransitionEvents, this.transitionDone);
     this.resetMargins()
   },
 
@@ -26,17 +28,17 @@ BOUNDLESS.Navigation = Backbone.View.extend({
   {
     this.$el.removeClass('segue')
     // We have to animate the marginRight instead of using 'resetMargins' to avoid an animation jump after its completed
-    this.$el.transition({ left : -1650}, BOUNDLESS.AnimationDuration, 'easeInOutQuad' )
-      .find('li').transition({marginRight: 30 }, BOUNDLESS.AnimationDuration )
+    //this.$el.transition({ left : -1650}, BOUNDLESS.AnimationDuration, 'easeInOutQuad' )
+    //  .find('li').transition({marginRight: 30 }, BOUNDLESS.AnimationDuration )
     this.hidden = true
     // Allows for clicking any part of the navigation tile
     // Protected by an event for browser back/forward navigation
-    if ( e ) BOUNDLESS.router.navigate( $(e.currentTarget).data().route, { trigger: true} )
+    //if ( e ) BOUNDLESS.router.navigate( $(e.currentTarget).data().route, { trigger: true} )
   },
 
   segueIn: function( e ) {
     this.$el.addClass('segue')
-    this.$el.transition({ left : -230 }, BOUNDLESS.AnimationDuration, 'easeInOutQuad', this.bounce )
+    //this.$el.transition({ left : -230 }, BOUNDLESS.AnimationDuration, 'easeInOutQuad', this.bounce )
     this.hidden = false
   },
 
@@ -46,6 +48,18 @@ BOUNDLESS.Navigation = Backbone.View.extend({
       if ( this.hidden && ! Backbone.history.fragment ) this.segueIn()
       if ( ! this.hidden && Backbone.history.fragment.length ) this.segueOut()
       if ( Backbone.history.fragment.length ) this.$el.removeClass( 'segue' )
+  },
+
+  transitionDone: function(event) {
+    if (event.originalEvent.propertyName == 'translateX'){
+      if (this.hidden) {
+        //trigger view stuff here
+      }
+      else {
+        this.bounce();
+        //trigger default view stuff here
+      }
+    }
   },
 
   bounce : function()
