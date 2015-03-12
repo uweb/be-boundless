fbfbce6cdab556e6e2b9390aef3901a80a758baa
Jon Swanson <jswan2@uw.edu>
2015-03-12 15:05:31 -0700

    removed the body scrolling stuff. Looks nice, but can hide the needed button

--- js/interaction.js
+++ js/interaction.js
@@ -38,6 +38,7 @@ BOUNDLESS.Navigation = Backbone.View.extend({
       BOUNDLESS.router.navigate( $(e.currentTarget).data().route, { trigger: true} );
       if (BOUNDLESS.mobile.is_mobile){
         this.$navwrap.find('li').not(e.currentTarget).addClass('faded');
+        // TODO: velocity doesn't seem to understand scrolling as an animation
         this.$navwrap.animate({scrollLeft : (this.$navwrap.scrollLeft()+$(e.currentTarget).offset().left - 20)}, 500);
       }
     }
@@ -46,9 +47,9 @@ BOUNDLESS.Navigation = Backbone.View.extend({
   complete : function()
   {
     // todo: more sublte way to implementing this
-    if (BOUNDLESS.mobile.is_mobile){
-      $('html,body').animate({scrollTop: 0}, 400);
-    }
+    // if (BOUNDLESS.mobile.is_mobile){
+    //  $('html,body').animate({scrollTop: 0}, 400);
+    //}
     this.hidden = true
     this.resetMargins()
     this.$homepage.addClass('blur')
@@ -58,9 +59,9 @@ BOUNDLESS.Navigation = Backbone.View.extend({
   segueIn: function( e ) {
     this.$homepage.removeClass('blur')
     this.$el.addClass('segue')
-    if (BOUNDLESS.mobile.is_mobile){
-      $('html,body').animate({scrollTop: $('#slide').height() - BOUNDLESS.mobile.win_height }, 400);
-    }
+    //if (BOUNDLESS.mobile.is_mobile){
+    //  $('html,body').animate({scrollTop: $('#slide').height() - BOUNDLESS.mobile.win_height }, 400);
+    //}
     // Given the iffyness of the clip mask a delay may be the more robust cross-browser solution
     //this.$el.velocity( "reverse", {complete : this.bounce }) //removed delay: 600
     this.$el.velocity({ translateX: '0%' }, BOUNDLESS.AnimationDuration, 'easeInOutQuad', this.bounce );
