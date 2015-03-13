d708b404d2af2375794d8aceb29f4bd2332640d1
Dane Odekirk <dizzzy@uw.edu>
2015-03-13 11:15:11 -0700

    Fixed mobile bugs: loading animation, clipping of naviation, reverted navigation to previously functioning version.

--- js/interaction.js
+++ js/interaction.js
@@ -5,8 +5,6 @@ BOUNDLESS.Navigation = Backbone.View.extend({
 
   hidden : false,
 
-  message : '#message',
-
   events : {
     'click li' : 'segueOut',
     'click .show-nav' : 'segueIn',
@@ -19,8 +17,7 @@ BOUNDLESS.Navigation = Backbone.View.extend({
      'complete',
      'resetMargins'
      )
-    this.$toggle = this.$('.show-nav')
-    this.$navwrap = this.$('#nav-wrap')
+    this.$navigationItems = this.$el.find('li')
     this.$homepage = $('#boundless-slide')
 
     this.resetMargins()
@@ -28,52 +25,31 @@ BOUNDLESS.Navigation = Backbone.View.extend({
 
   segueOut : function( e )
   {
-
-    if ( e && ! $( e.currentTarget ).data().route ) return false;
-
     this.$el.removeClass('segue')
+    // We have to animate the marginRight instead of using 'resetMargins' to avoid an animation jump after its completed
+    this.$el.velocity({ translateZ: 0, translateX: -235 * this.$navigationItems.length  }, BOUNDLESS.AnimationDuration, 'easeInOutQuad', this.complete )
 
-    this.$el.velocity({ translateX: '-100%'}, BOUNDLESS.AnimationDuration, 'easeInOutQuad', this.complete )
-
+    this.hidden = true
     // Allows for clicking any part of the navigation tile
     // Protected by an event for browser back/forward navigation
-    if ( e ){
-      BOUNDLESS.router.navigate( $(e.currentTarget).data().route, { trigger: true} );
-      if (BOUNDLESS.mobile.is_mobile){
-        this.$navwrap.find('li').not(e.currentTarget).addClass('faded');
-        // TODO: velocity doesn't seem to understand scrolling as an animation
-        this.$navwrap.animate({scrollLeft : (this.$navwrap.scrollLeft()+$(e.currentTarget).offset().left - 20)}, 500);
-      }
-    }
+    if ( e ) BOUNDLESS.router.navigate( $(e.currentTarget).data().route, { trigger: true} )
   },
 
   complete : function()
   {
     // todo: more sublte way to implementing this
-    // if (BOUNDLESS.mobile.is_mobile){
-    //  $('html,body').animate({scrollTop: 0}, 400);
-    //}
-    this.hidden = true
     this.resetMargins()
     this.$homepage.addClass('blur')
     this.trigger('complete')
   },
 
   segueIn: function( e ) {
-    if (e){
-      e.preventDefault();
-    }
-    if (this.hidden){
-      this.$homepage.removeClass('blur')
-      this.$el.addClass('segue')
-      //if (BOUNDLESS.mobile.is_mobile){
-      //  $('html,body').animate({scrollTop: $('#slide').height() - BOUNDLESS.mobile.win_height }, 400);
-      //}
-      // Given the iffyness of the clip mask a delay may be the more robust cross-browser solution
-      //this.$el.velocity( "reverse", {complete : this.bounce }) //removed delay: 600
-      this.$el.velocity({ translateX: '0%' }, BOUNDLESS.AnimationDuration, 'easeInOutQuad', this.bounce );
-      BOUNDLESS.router.navigate('', {trigger:true});
-    }
+     this.$homepage.removeClass('blur')
+     this.$el.addClass('segue')
+     // Given the iffyness of the clip mask a delay may be the more robust cross-browser solution
+     // this.$el.velocity( "reverse", {complete : this.bounce, delay: 600 })
+     this.$el.velocity({ translateX: '0%' }, BOUNDLESS.AnimationDuration, 'easeInOutQuad', this.bounce );
+     this.hidden = false
   },
 
   segue : function()
@@ -88,16 +64,13 @@ BOUNDLESS.Navigation = Backbone.View.extend({
   {
      // Animate is used for the easeOutElastic easing
      // TODO: why doesn't velocity understand the easing?
-     this.hidden = false
-     //prevent weird in-between state
-     this.$homepage.removeClass('blur');
-     this.$el.find('li').animate({ marginRight: 20 }, 2 * BOUNDLESS.AnimationDuration, 'easeOutElastic' ).removeClass('faded');
+      this.$navigationItems.animate({ marginRight: 20 }, 2 * BOUNDLESS.AnimationDuration, 'easeOutElastic' )
   },
 
   // Resets the margins of the navigation LI's to create the elastic bounce in effect
   resetMargins : function()
   {
-    this.$el.find('li').css({ marginRight: 30 })
+    this.$navigationItems.css({ marginRight: 30 })
   }
 
 
