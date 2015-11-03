8fed6cc0a269b3fe77ddd2e7d11595cad424a732
Kilian <kilianf@uw.edu>
2015-11-03 11:27:23 -0800

    Resolving broken map image

--- js/boundless.js
+++ js/boundless.js
@@ -1602,7 +1602,7 @@ BOUNDLESS.Map.InfoWindow.prototype.render = function( marker )
   this.div.innerHTML = _.template(
     '<div class="image-mask">' +
       '<div class="image">' +
-         '<img src="<%= info.get("image") %>" title="<%= info.get("title") %>" />' +
+         '<img src="<%= _.first(info.get("image")) %>" title="<%= info.get("title") %>" />' +
         '<h3><%= info.get("title") %></h3>' +
       '</div>' +
     '</div>' +
