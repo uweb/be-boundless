9b966e554145db735960b7524ee50478d5523b90
Nick Winkelbauer <nwinkelb@uw.edu>
2016-03-22 12:24:56 -0700

    instagram first

--- js/boundless.js
+++ js/boundless.js
@@ -1409,8 +1409,8 @@ BOUNDLESS.Gallery = Backbone.View.extend({
 
   render : function()
   {
-
-    this.$el.prepend( _.template( this.templateInstagram, {images : this.instagram.toJSON() }) )
+    console.log(this.$el)
+    this.$el.append( _.template( this.templateInstagram, {images : this.instagram.toJSON() }) )
     BOUNDLESS.app.set( 'instagram', true )
     this.$el.attr('data-height', 10000)
 
