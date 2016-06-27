feab69d7d36e2f99893e443e17f5bd4d8cae348b
Kilian <kilianf@uw.edu>
2016-06-27 15:30:32 -0700

    Added campaign page

---
+++ page-campaign.php
@@ -0,0 +1,42 @@
+<?php get_header(); ?>  
+    
+    <?php uw_mobile_front_page_menu('bighero'); ?>
+
+
+    
+
+    <main id='main_content'>
+    
+        
+    <section class="text-block no-white-bg">
+         
+    </section>
+
+
+    
+
+    </main> 
+
+      
+      <!-- **** CSS **** -->
+      <link rel='stylesheet' href='<?php echo get_stylesheet_directory_uri() . '/immersive-stories/css/common.css' ?>' type='text/css' media='all' />
+      <link rel='stylesheet' href='<?php echo get_stylesheet_directory_uri() . '/immersive-stories/css/farmer-brown.css' ?>' type='text/css' media='all' />
+      
+      <!-- **** JS **** -->      
+      <!-- <script src="<?php echo get_stylesheet_directory_uri() . '/immersive-stories/js/hunt.min.js' ?>" type="text/javascript"></script> -->
+      <!-- <script src="<?php echo get_stylesheet_directory_uri() . '/immersive-stories/js/jquery.animateNumber.min.js' ?>" type="text/javascript"></script> -->
+
+      <script src="//cdnjs.cloudflare.com/ajax/libs/gsap/1.18.5/TweenMax.min.js"></script>
+      <script src="//cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.5/ScrollMagic.min.js"></script>
+      <script src="//cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.5/plugins/animation.gsap.min.js"></script>
+      <script src="//cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.5/plugins/debug.addIndicators.min.js"></script>
+
+      <script src="<?php echo get_stylesheet_directory_uri() . '/immersive-stories/js/common.min.js' ?>" type="text/javascript"></script>
+      <script src="<?php echo get_stylesheet_directory_uri() . '/immersive-stories/js/farmer-brown.min.js' ?>" type="text/javascript"></script>
+
+
+
+<?php get_footer(); ?>
+
+
+
