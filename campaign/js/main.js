fcc0826ed49fd80d3d5d3df58ebe74ad63228d77
Kilian <kilianf@uw.edu>
2016-09-07 09:15:33 -0700

    Change scroll speed to 40

--- campaign/js/main.js
+++ campaign/js/main.js
@@ -353,7 +353,7 @@ $(function(){
    	$('html, body, *').mousewheel(function(e, delta) {
         // multiplying by 40 is the sensitivity, 
         // increase to scroll faster.
-        this.scrollLeft -= (delta * 5);
+        this.scrollLeft -= (delta * 40);
         e.preventDefault();
     });
 
