d2186f0f20321a2a311cfcc71766e225aa1fb124
Kilian <kilianf@uw.edu>
2016-09-12 17:28:57 -0700

    Audio test 3

--- immersive-stories/js/common.js
+++ immersive-stories/js/common.js
@@ -2,26 +2,87 @@ var isMobile = window.matchMedia("only screen and (max-width: 768px)");
 
 
 
-// Read more link
+$(function(){
 
-$('.read-more').on('click', function(e){
-  e.preventDefault();
-  $('html, body').animate({
-      scrollTop: (document.getElementsByTagName('main')[0].getBoundingClientRect().top - 80)
-    }, {
-      duration: 1000
+  alert(2)
+
+  // Audio Player
+  $(".audio-ctrl").each(function(){
+  
+    var $this = $(this);
+  
+    // Set ARIA attribute to false
+    $this.attr("aria-pressed","false");
+  
+    $this.click(function(e){
+      e.preventDefault();
+  
+      // Pause all currently playing
+      var mp3s = document.body.getElementsByTagName('audio');    
+      for (var i = 0; i < mp3s.length; i++ ) {
+        mp3s[i].pause()
+      }
+  
+  
+     //$('audio').each(function(e){
+     //  console.log($(this))
+     //})
+  
+  
+      var $audio = $(e.target).closest('.audio'),
+          audioEl = $audio.find('audio')[0]
+          
+      $audio.addClass('active-audio');
+      $audio.find('button').attr("aria-pressed","false")
+      $this.attr("aria-pressed","true");
+  
+      if($this.hasClass("audio-play")){
+        $('audio')[0].pause();
+        setTimeout(function(){
+          audioEl.play();
+        },1000);
+      }
+      if($this.hasClass("audio-pause")){
+        audioEl.pause();              
+      }
+      if($this.hasClass("audio-read")){
+        $audio.find(".audio-transcript").removeClass("visually-hidden").focus();
+        $audio.toggleClass("trans");
+  
+        if($this.hasClass('active-transcript')){
+          $audio.find(".audio-transcript").addClass("visually-hidden")
+          $this.removeClass('active-transcript');
+          $audio.toggleClass("trans");
+        } else {                
+          $this.addClass('active-transcript'); 
+        }              
+      } 
+    });
   });
-})
+  
+  // Read more link
+  
+  $('.read-more').on('click', function(e){
+    e.preventDefault();
+    $('html, body').animate({
+        scrollTop: (document.getElementsByTagName('main')[0].getBoundingClientRect().top - 80)
+      }, {
+        duration: 1000
+    });
+  })
+  
+  
+  // Video pause
+  $( "#pause" ).click(function() {     
+    var vid =  $( "#intro-vid video" )
+    if ( vid.get(0).paused ) {
+    	vid.get(0).play() } 
+    else { 
+    	vid.get(0).pause();
+    }
+    $('#intro-vid').toggleClass('paused');
+  });
 
 
-// Video pause
-$( "#pause" ).click(function() {     
-  var vid =  $( "#intro-vid video" )
-  if ( vid.get(0).paused ) {
-  	vid.get(0).play() } 
-  else { 
-  	vid.get(0).pause();
-  }
-  $('#intro-vid').toggleClass('paused');
 });
 
--- immersive-stories/js/education.js
+++ immersive-stories/js/education.js
@@ -1,59 +1,182 @@
 $(function(){
 
-  alert('1')
-// Audio Player
-$(".audio-ctrl").each(function(){
+  var controllerEducation = new ScrollMagic.Controller()
 
-  var $this = $(this);
+  var zoomMap = new TimelineMax ()
+      .add([
+        TweenMax.to(".school-pic", 1, { y: '-50%', ease: Power0.easeOut }),
+        TweenMax.to(".blurb1", 0.5, { y: '-100%', opacity: 0 }, 1),
+        TweenMax.to(".blurb2", 0.5, { y: '0', opacity: 1 })
+      ])
 
-  // Set ARIA attribute to false
-  $this.attr("aria-pressed","false");
+    // build scenes
+    var schoolZoom = new ScrollMagic.Scene({
+        triggerElement: ".school-zoom",
+        triggerHook: 0.5,
+        duration: "50%",
+      })
+      .setTween(zoomMap)
+      // .on("progress", function (event) {
+      //      window.requestAnimationFrame(function(){
+      //       scollPlay(event.progress * 10)
+      //      })
+      //      console.log(event.progress * 10)
+      // })
+      // .setPin(".school-zoom")
+      // .addIndicators()
+      .addTo(controllerEducation);
 
-  $this.click(function(e){
-    e.preventDefault();
+    var studentpic1 = new ScrollMagic.Scene({
+      triggerElement: "#pic1",
+      triggerHook: 0.4,
+      duration: '25%',
+    })
+    .setTween(TweenMax.from('#pic1 .pic-2', 1, { x: '100%', ease: Power0.easeIn }))
+    //.addIndicators()
+    .addTo(controllerEducation);
 
-    // Pause all currently playing
-    var mp3s = document.body.getElementsByTagName('audio');    
-    for (var i = 0; i < mp3s.length; i++ ) {
-      mp3s[i].pause()
+
+    var studentpic2 = new ScrollMagic.Scene({
+      triggerElement: "#pic2",
+      triggerHook: 0.6,
+      duration: '15%',
+    })
+    .setTween(TweenMax.to('#pic2 .pic-2', 1, { x: '100%', ease: Power0.easeIn }))
+    //.addIndicators()
+    .addTo(controllerEducation);
+
+
+    var intro = new ScrollMagic.Scene({
+      triggerElement: '#intro-slide',
+      triggerHook: 0,
+      duration: '100%',
+    })
+    .setPin('.intro', {pushFollowers: false})
+    .setTween('#intro-text', 1, { opacity: 0, transform: 'translateY(-100px)', ease: Power0.easeIn })
+    .addTo(controllerEducation);
+
+
+    var mapTrigger = new ScrollMagic.Scene({
+      triggerElement: '#mapid',
+      triggerHook: 1
+    })
+    .on("start", function(){
+      if (!isMobile.match) {
+        mapInit();
+      }
+    })
+    .reverse(false)
+    .addTo(controllerEducation);
+
+
+    var videoTrigger = new ScrollMagic.Scene({
+      triggerElement: '#video',
+      triggerHook: 1
+    })
+    .on("start", videoPlay)
+    .reverse(false)
+    .addTo(controllerEducation);
+
+
+
+
+
+
+  // Control video frame by scrolling: 
+
+   // var frameNumber = 0, // start video at frame 0
+        // lower numbers = faster playback
+   //      playbackConst = 50, 
+        // select video element         
+   //      vid = document.getElementById('schoolVideo'); 
+        // var vid = $('#v0')[0]; // jquery option
+
+
+    // Use requestAnimationFrame for smooth playback
+    // function scollPlay(e){  
+    //       vid.currentTime  = e;
+    // }
+
+    function videoPlay(){
+      var videoSrc = "https://www.youtube.com/embed/l3SLvfzwVkU?autoplay=1&rel=0&amp;showinfo=0&amp",          
+          $video = $('#video'),
+          $body = $("body"),
+          boundlessVideo = document.getElementById("boundless-video"),
+          videoHTML = 
+            '<button class="close-video"><span class="top"></span><span class="left"></span><span class="bottom"></span></button>' +
+            '<div id="youtube-video">' + 
+            '<iframe title="YouTube video" id="embedVid" width=' + $video.width() + ' height=' + $video.height() + ' src="' + videoSrc + '" frameborder="0" allowfullscreen autoplay></iframe>' +
+            '</div>';
+
+      if (!!navigator.userAgent.match(/iPhone/i)) {
+
+        $body.toggleClass('iPhone');
+        boundlessVideo.innerHTML = videoHTML;
+
+      } else {
+
+        $(".play").click(function(e){
+           e.preventDefault();     
+           boundlessVideo.innerHTML = videoHTML;
+
+           setTimeout( function(){
+             $('#video iframe')[0].focus()
+           }, 500 );
+
+           $(".close-video").click(function(){
+             $(".play").removeClass("hidden");
+             $body.toggleClass("playing");
+             boundlessVideo.innerHTML = '';
+           });
+
+          $body.toggleClass("playing");
+        });
+
+      }
     }
 
 
-   //$('audio').each(function(e){
-   //  console.log($(this))
-   //})
-
-
-    var $audio = $(e.target).closest('.audio'),
-        audioEl = $audio.find('audio')[0]
-        
-    $audio.addClass('active-audio');
-    $audio.find('button').attr("aria-pressed","false")
-    $this.attr("aria-pressed","true");
-
-    if($this.hasClass("audio-play")){
-      $('audio')[0].pause();
-      setTimeout(function(){
-        audioEl.play();
-      },1000);
+  // Audio interviews
+  $('#people li').on('click', function(){
+    var $this = $(this);
+    if( !$this.hasClass('active') ){
+      $this.addClass('active');
+    } else {
+      $this.removeClass('active');
     }
-    if($this.hasClass("audio-pause")){
-      audioEl.pause();              
-    }
-    if($this.hasClass("audio-read")){
-      $audio.find(".audio-transcript").removeClass("visually-hidden").focus();
-      $audio.toggleClass("trans");
+    $this.parent().children('li').not($this).removeClass('active');
+  })
 
-      if($this.hasClass('active-transcript')){
-        $audio.find(".audio-transcript").addClass("visually-hidden")
-        $this.removeClass('active-transcript');
-        $audio.toggleClass("trans");
-      } else {                
-        $this.addClass('active-transcript'); 
-      }              
-    } 
-  });
-});
+
+  // Focus on button triggers 
+  $('.audio-play').bind("focus blur", function(event){
+       event.stopPropagation();
+       if(event.type == "focus")  {
+        $(event.target).closest('li').toggleClass('active-focus');
+       } 
+       else if(event.type == "blur") {
+        $(event.target).closest('li').toggleClass('active-focus');
+       }
+   });
+
+  // Lightbox plugin
+
+  MediumLightbox('figure.zoom-effect');    
+
+  var img = document.getElementsByClassName('img')
+
+  $(img).each(function(index){
+    var imgIndex = img[index],
+        $this = $(this),
+        hasCaption = false;
+
+    imgIndex.addEventListener("mouseover", function(){
+      if (!hasCaption) {
+        $this.after('<p>' + $this.attr('alt') + '</p>')
+        hasCaption = true;
+      }
+    })
+  })
 
 
 
