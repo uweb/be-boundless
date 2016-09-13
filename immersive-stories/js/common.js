48e4ac230de66669b86b1d0bcb388fba803a7c31
Kilian <kilianf@uw.edu>
2016-09-13 10:37:32 -0700

    Audio test 4

--- immersive-stories/js/common.js
+++ immersive-stories/js/common.js
@@ -5,58 +5,48 @@ var isMobile = window.matchMedia("only screen and (max-width: 768px)");
 $(function(){
 
   // Audio Player
-  $(".audio-ctrl").each(function(){
-  
-    var $this = $(this);
-  
-    // Set ARIA attribute to false
-    $this.attr("aria-pressed","false");
-  
-    $this.click(function(e){
-      e.preventDefault();
-  
-      // Pause all currently playing
-      var mp3s = document.body.getElementsByTagName('audio');    
-      for (var i = 0; i < mp3s.length; i++ ) {
-        mp3s[i].pause()
-      }
-  
-  
-     //$('audio').each(function(e){
-     //  console.log($(this))
-     //})
-  
-  
-      var $audio = $(e.target).closest('.audio'),
-          audioEl = $audio.find('audio')[0]
-          
-      $audio.addClass('active-audio');
-      $audio.find('button').attr("aria-pressed","false")
-      $this.attr("aria-pressed","true");
-  
-      if($this.hasClass("audio-play")){
-        $('audio')[0].pause();
-        setTimeout(function(){
-          audioEl.play();
-        },1000);
-      }
-      if($this.hasClass("audio-pause")){
-        audioEl.pause();              
-      }
-      if($this.hasClass("audio-read")){
-        $audio.find(".audio-transcript").removeClass("visually-hidden").focus();
-        $audio.toggleClass("trans");
-  
-        if($this.hasClass('active-transcript')){
-          $audio.find(".audio-transcript").addClass("visually-hidden")
-          $this.removeClass('active-transcript');
-          $audio.toggleClass("trans");
-        } else {                
-          $this.addClass('active-transcript'); 
-        }              
-      } 
-    });
-  });
+        // Audio Player
+        alert(1)
+        $(".audio-ctrl").each(function(){
+
+          var $this = $(this);
+
+          // Set ARIA attribute to false
+          $this.attr("aria-pressed","false");
+
+          $this.click(function(e){
+            e.preventDefault();
+
+            // Pause all currently playing
+            document.body.getElementsByTagName('audio')[0].pause()
+
+            var $audio = $(e.target).closest('.audio'),
+                audioEl = $audio.find('audio')[0]
+                
+            $audio.addClass('active-audio');
+            $audio.find('button').attr("aria-pressed","false")
+            $this.attr("aria-pressed","true");
+
+            if($this.hasClass("audio-play")){
+              setTimeout(function(){
+                audioEl.play();
+              },1000);
+            }
+            if($this.hasClass("audio-pause")){
+              audioEl.pause();              
+            }
+            if($this.hasClass("audio-read")){
+              $audio.find(".audio-transcript").removeClass("visually-hidden").focus();
+              if($this.hasClass('active-transcript')){
+                $audio.find(".audio-transcript").addClass("visually-hidden")
+                $this.removeClass('active-transcript');
+              } else {                
+                $this.addClass('active-transcript'); 
+              }              
+            } 
+          });
+        });
+
   
   // Read more link
   
