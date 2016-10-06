38dbec624fbb279bfc9f931e86464b00fcc33493
Kilian <kilianf@uw.edu>
2016-10-06 16:37:55 -0700

    Added pics

---
+++ footer-campaign.php
@@ -0,0 +1,14 @@
+<nav role="navigation" aria-label="footer links">
+    <ul class="footer-links">
+        <li><a href="http://www.uw.edu/accessibility">Accessibility</a></li>
+        <li><a href="http://uw.edu/home/siteinfo/form">Contact Us</a></li>
+        <li><a href="http://www.washington.edu/jobs">Jobs</a></li>
+        <li><a href="http://www.washington.edu/safety">Campus Safety</a></li>
+        <li><a href="http://myuw.washington.edu/">My UW</a></li>
+        <li><a href="http://www.washington.edu/rules/wac">Rules Docket</a></li>
+        <li><a href="http://www.washington.edu/online/privacy/">Privacy</a></li>
+        <li><a href="http://www.washington.edu/online/terms/">Terms</a></li>
+    </ul>
+</nav>
+
+<p class="dateMarker">&copy; <?php echo date("Y"); ?> University of Washington  |  Seattle, WA</p>
