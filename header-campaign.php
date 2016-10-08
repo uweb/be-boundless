50e8ff45e8660fa79bde9cec3b6f164ad81fd75c
Kilian <kilianf@uw.edu>
2016-10-08 13:01:41 -0700

    Adjusted Boundless logo and position

--- header-campaign.php
+++ header-campaign.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8"> 
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-  <meta name="keywords" content="ScrollMagic, example, scrolling, attaching, scrollbar, horizontal, landscape" />
+  <meta name="keywords" content="University of Washington Boundless Campaign" />
   <!-- <meta name="viewport" content="width=device-width"> -->
   <!-- <meta name="viewport" content="initial-scale=1 user-scalable=no width=device-width"> -->
   <meta name="viewport" content="initial-scale=.95 width=device-width maximum-scale=1 user-scalable=no" />
