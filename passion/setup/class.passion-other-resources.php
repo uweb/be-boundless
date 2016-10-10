288c66991699cd05240b977fdb1dfc381d7188b4
Nick Winkelbauer <nwinkelb@uw.edu>
2016-10-10 11:05:57 -0700

    Bug Fix: Removed function header call for tiles.js

--- passion/setup/class.passion-other-resources.php
+++ passion/setup/class.passion-other-resources.php
@@ -1,13 +1,13 @@
 <?php
 
-add_action('init', 'load_other_resources');
+// add_action('init', 'load_other_resources');
 
-function load_other_resources() {
-	wp_enqueue_script('jquery');
-	wp_register_script('tiles-js', get_stylesheet_directory_uri() . '/passion/js/tiles.js');
-	wp_enqueue_script('tiles-js');
-	wp_register_style('tiles-style', get_stylesheet_directory_uri() . '/passion/css/tiles.css');
-	wp_enqueue_style('tiles-style');
-}
+// function load_other_resources() {
+// 	wp_enqueue_script('jquery');
+// 	wp_register_script('tiles-js', get_stylesheet_directory_uri() . '/passion/js/tiles.js');
+// 	wp_enqueue_script('tiles-js');
+// 	wp_register_style('tiles-style', get_stylesheet_directory_uri() . '/passion/css/tiles.css');
+// 	wp_enqueue_style('tiles-style');
+// }
 
 ?>
