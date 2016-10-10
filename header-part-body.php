a8ec77415b7b376ee4774a0cfe11acc936cc6d73
Nick Winkelbauer <nwinkelb@uw.edu>
2016-10-10 11:38:23 -0700

    Restructured Header

---
+++ header-part-body.php
@@ -0,0 +1,31 @@
+<header role="banner">
+  <div id="campaign-header" class="active" role="navigation">
+
+    <a class="home" href="http://www.uw.edu">Home</a>
+
+    <div tabindex="-1" class="thin" aria-hidden="true">
+      <a href="http://www.uw.edu">Support the campaign</a>
+    </div>
+    <div tabindex="-1" class="thick">
+      <ul>
+        <li><a href="campaign">Home</a></li>
+        <li><a href="experience">Experience</a></li>
+        <li><a href="leadership">About the Campaign</a></li>
+        <li><a href="passion">Find Your Passion</a></li>
+        <li><button id="give-now">Give now</button></li>
+      </ul>        
+    </div>
+    <button id="campaign-expand">Toggle menu
+      <div aria-hidden="true" id="ham">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </button>
+  </div>
+
+
+  <div id="give-iframe"></div>
+
+</header>
--- header-passion.php
+++ header-passion.php
@@ -1,5 +1,5 @@
 <?php get_header( "part-head" ); ?>
 
-<body id="immersive" <?php body_class(); ?>>
+<body id="passion" <?php body_class(); ?>>
 
 <?php get_header( "part-body" ); ?>
