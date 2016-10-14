b43f038ba068c1a99b791616262b8efb0ee2db4e
Kilian <kilianf@uw.edu>
2016-10-14 16:06:34 -0700

    Medicine updates

--- immersive-stories/js/medicine.js
+++ immersive-stories/js/medicine.js
@@ -33,72 +33,9 @@ $(function(){
       body.classList.toggle('accord-open');
     });
 
-
-    // Add video for video scroll
-
-    //  $('.video-auto').each(function(index,element){
-    //     var $this = $(element);
-    //     var that = element;
-    //     var videoAutoTrigger = new ScrollMagic.Scene({
-    //       triggerElement: element,
-    //       triggerHook: 1
-    //     })
-    //     .on("start", function(){
-    //         videoAdder(element, $this)
-    //     })
-    //     .reverse(false)
-    //     .addTo(controllerEngineering);
-    // })
-
-
-    // function videoAdder(el, $el){
-    //     $el.addClass('active');
-    //     // Dynamically add video
-    //     var vid = $el.find('video'),
-    //         atts = el.getAttribute('data-vid'),                        
-    //         mp4 = atts + '.mp4',
-    //         webm = atts + '.webm'
-
-    //     $el.find('.mp4').attr('src', mp4)
-    //     $el.find('.webm').attr('src', webm)
-    //     vid[0].load();
-    // }
-
-
-  // // Audio interviews
-  // $('#people li').on('click', function(){
-  //   var $this = $(this);
-  //   if( !$this.hasClass('active') ){
-  //     $this.addClass('active');
-  //   } else {
-  //     $this.removeClass('active');
-  //   }
-  //   $this.parent().children('li').not($this).removeClass('active');
-  // })
-
-
-  // // Focus on button triggers 
-  // $('.audio-play').bind("focus blur", function(event){
-  //      event.stopPropagation();
-  //      if(event.type == "focus")  {
-  //       $(event.target).closest('li').toggleClass('active-focus');
-  //      } 
-  //      else if(event.type == "blur") {
-  //       $(event.target).closest('li').toggleClass('active-focus');
-  //      }
-  //  });
-
-  // $('.medicine-slideshow').slick({
-  //   lazyLoad: 'ondemand',
-  //   useTransform: true,
-  //   dots: true,
-  //   infinite: false,
-  //   speed: 300,
-  //   slidesToShow: 1,
-  //   slidesToScroll: 1,
-  // });
-  
-
+    document.getElementsByClassName('audio-play')[0].addEventListener('click',function(){
+      document.getElementsByClassName('background-profile')[0].classList.toggle('active');
+    })
 
 
 
