dd5c48dff65bbdb3adb3720aa76c1ff7511dbc00
Nick Winkelbauer <nwinkelb@uw.edu>
2016-10-18 14:15:00 -0700

    Bug Fix: PAssion - resized search tiles for full screen. need work for mobile sizing

--- passion/js/tiles.dev.js
+++ passion/js/tiles.dev.js
@@ -157,6 +157,7 @@ $(window).load(function(){
         $prevSelect = $('.grid-item.open:not(.search-more):not(.filter-item)');
         $prevSelect.removeClass('open');
         $grid.isotope( 'updateSortData', $prevSelect );
+        $grid.isotope();
       })
 
       //DO WE WANT THIS???
@@ -339,6 +340,7 @@ $(window).load(function(){
       $prevSelect = $('.grid-item.open:not(.search-more):not(.filter-item)');
       $prevSelect.removeClass('open');
       $grid.isotope( 'updateSortData', $prevSelect ); 
+      // $grid.isotope();
       $.getJSON('http://service.gifts.washington.edu/OnlineAllocation/Search/' + $searchTerm + '?callback=?', function(data) {
           //console.log(data);
           //$('.grid-item.search-more').addClass('hide');
@@ -367,6 +369,13 @@ $(window).load(function(){
                 '      <div class="give-button">' +
                 '        <a href="#" class="give-link" data-code="' + alloc + '">Give Now</a>' +
                 '      </div>' +
+                '      <button class="empty-fund">' +
+                '        Close current fund' +
+                '        <svg aria-hidden="true" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="34.147px" height="37.833px" viewBox="0 0 34.147 37.833" enable-background="new 0 0 34.147 37.833" xml:space="preserve">' +
+                '          <line fill="none" stroke="#A7A9AC" stroke-width="2" stroke-miterlimit="10" x1="9.616" y1="8.449" x2="31.782" y2="30.614"></line>' +
+                '          <line fill="none" stroke="#A7A9AC" stroke-width="2" stroke-miterlimit="10" x1="31.782" y1="8.449" x2="9.616" y2="30.614"></line>' +
+                '        </svg>' +
+                '      </button>' +
                 '    </div>' +
                 '  </div>' +
                 '</li>'));
