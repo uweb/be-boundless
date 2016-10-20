7ff80085fc4d6b39895a7fc7bd1dfca29f7459fa
Kilian <kilianf@uw.edu>
2016-10-20 11:41:17 -0700

    Text updates

--- page-about.php
+++ page-about.php
@@ -41,7 +41,7 @@
                   <p>$<span id="camp-current">3.1</span> billion <b></b></p>
                 </li>
                 <li>
-                  <div><p><b>Goal</b> $<span id="camp-goal">4.0</span> billion</p></div>
+                  <div><p><b>Goal</b> $<span id="camp-goal">5.0</span> billion</p></div>
                 </li>
               </ul>
             </div>
