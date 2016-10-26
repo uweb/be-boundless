52d5a21f3ff3cc63c7c8799b2fe59175e732ec37
Kilian <kilianf@uw.edu>
2016-10-26 16:03:50 -0700

    Updated header for OG image

--- header-campaign-open.php
+++ header-campaign-open.php
@@ -10,19 +10,14 @@
   <title> <?php wp_title(' | ',TRUE,'right'); bloginfo('name'); ?> </title>
   <meta property="og:title" content="<?php the_title(''); ?>" />
   <meta property="og:url" content="<?php echo get_permalink(); ?>" />
-  
-  <?php if (has_post_thumbnail( $post->ID ) ): ?>
-  <?php $image = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'single-post-thumbnail'); ?>
-  <meta property="og:image" content="<?php echo $image[0]; ?>" />
-  <?php else : ?>
-  <meta property="og:image" content="<?php echo get_stylesheet_directory_uri() . '/campaign/img/og_image.jpg'; ?>" 
-  <?php endif; ?> 
-
+  <meta property="og:description" content="What you care about can change the world. When you support your passions by giving through the UW, you’re providing students and faculty with the resources they need to learn, collaborate and research in one of the most innovative places on earth." />
+  <meta property="og:image" content="http:<?php echo get_stylesheet_directory_uri() . '/campaign/img/og-image.jpg' ?>" />
+      
+      
   <link rel='stylesheet' id='uw-master-css'  href='<?php echo bloginfo("template_directory") . '/style.css' ?>' type='text/css' media='all' />
   <link rel='stylesheet' id='google-font-open-css'  href='https://fonts.googleapis.com/css?family=Open+Sans%3A400italic%2C700italic%2C400%2C700&#038;' type='text/css' media='all' />
   <link rel='stylesheet' id='uw-style-css'  href='<?php echo get_stylesheet_directory_uri() . '/style.css' ?>' type='text/css' media='all' />
   <script type='text/javascript' src='https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js'></script>
   <!-- <script type='text/javascript' src='https://code.jquery.com/ui/1.12.0-rc.2/jquery-ui.min.js'></script> -->
-
 
 
