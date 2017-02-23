0999215933cf1c1d581514972276a2eb596232da
Kilian <kilianf@uw.edu>
2017-02-22 17:42:07 -0800

    Evans updates

--- page-evans-school-powering-policy.php
+++ page-evans-school-powering-policy.php
@@ -12,7 +12,7 @@
 
 <div class="intro" id="intro-vid">
     <div id="slide">
-      <h1>Powering policy at home and abroad</h1>  
+      <h1>Powering policy locally and globally</h1>  
       <hr> 
       <p>How can you change the world through the University of Washington? Two students at the Evans School of Public Policy &amp; Governance share their stories.
 </p>     
@@ -35,18 +35,18 @@
                 <div class="col-md-10 col-md-offset-1 copy">
                   
                     <p>The Evans School uses the world as their laboratory. Diving deep into pressing public policy and governance issues of today, the Evans School strives to create leaders who will solve the issues of tomorrow. </p>
-                    <p>From making agricultural investments in developing countries to passing laws in Washington state, research undertaken by Evans School faculty with assistance from graduate students informs policymaking around the world. </p>
-                    <p>Ph.D. student Hilary Wething and M.P.A. candidate Matthew Fowle are making the most of their experiences at the UW, thanks to the generosity of donors. They’re taking part in research opportunities with real-world influence: Wething studies the effects of Seattle’s historic minimum wage law, and Fowle is working to find evidence-based solutions to reduce poverty in third-world countries.</p>
+                    <p>From guiding investments in developing countries to shaping policy in Washington state, research undertaken by Evans School faculty with assistance from graduate students informs policymaking around the world. </p>
+                    <p>Ph.D. student Hilary Wething and M.P.A. candidate Matthew Fowle are making the most of their experiences at the UW. Thanks to the generosity of donors like you, both Wething and Fowle received scholarships that enable them to focus on academic pursuits and research with real-world influence: Wething studies the effects of Seattle’s historic minimum wage law, and Fowle is working to find evidence-based solutions to reduce poverty in developing countries.</p>
 
                     <div class="wide"><img alt="Photo of Hilary Wething" src="<?php echo get_stylesheet_directory_uri() . '/immersive-stories/img/evans-school/hilary.jpg' ?>"><p><span><b>Hilary Wething</b> / Research Assistant / City of Seattle Minimum Wage Study</span></p></div>
-                    <h2 class="slant"><span><span>Impact</span></span> at home</h2>
+                    <h2 class="slant"><span><span>Local</span></span> Impact</h2>
 
                     <p class="bolder"><b>The City of Seattle Minimum Wage Study</b></p>
                     
                     <p>In January 2014, Seattle made history when it became the first major city in the U.S. to adopt a $15 minimum wage. </p>
-                    <p>When the new law took effect the following year, policymakers were entering uncharted waters, as were workers and businesses. No one knew how the increase would affect people in a city with a skyrocketing cost of living.</p>
-                    <p>With the rest of the nation watching to see how it would unfold — and waiting to see if they should follow suit — the city of Seattle contracted with an interdisciplinary team of UW faculty from the Evans School, the School of Social Work and the School of Public Health to conduct a multi-year study of the new law and its effects.</p>
-                    <p>For researchers at the Evans School, the wage increase presented an unprecedented opportunity: the ability to track and study the effects of a new policy in real-time. </p>
+                    <p>When the new law took effect the following year, policymakers were entering uncharted waters, as were workers and businesses. No one knew how the increase would play out with real people in the real world.</p>
+                    <p>With the rest of the nation watching to see how it would unfold — and waiting to see if they should follow suit — the City of Seattle contracted with an interdisciplinary team of UW faculty from the Evans School, the School of Social Work and the School of Public Health to conduct a multi-year study of the new law and its effects.</p>
+                    <p>For researchers at the Evans School, the wage increase presented an unprecedented opportunity: the ability to track and study the effects of a new policy in real time. </p>
                     <p>“Often when you’re studying public policy, you either come in after a policy has been implemented, or your team helps design a program and then studies it from the beginning,” explains Heather Hill, an associate professor at the Evans School and co-investigator for the study. “But this is different. We get to evaluate as it happens.”                     </p>
                     <p>As the new wages were implemented, the researchers tracked potential bellwethers of the law’s impact, including wages, the number of hours employees worked and the prices of local goods. </p>
                     <p>But numbers can only say so much. </p>
@@ -87,8 +87,8 @@
           <!-- 50% -->
           <div class="col-b">
               <p>Do you think that the minimum wage law will have an impact on your life in some way?</p>
-              <div><blockquote><span>&ldquo;</span>Hopefully. I hope the minimum wage law also incentivizes companies to pay more even if they are already paying $15, just to maintain those pay structures [for other employees.]<span>&rdquo;</span></blockquote></div>
-              <small><b>Nikki McThewson</b> / Retail coordinator and shop mechanic</small>
+              <div><blockquote><span>&ldquo;</span>Yes. Besides my job here at Bike Works, I deliver food on my bike as a second source of income. With a higher wage here, I can spend more time with my family instead of working multiple jobs.<span>&rdquo;</span></blockquote></div>
+              <small><b>Nikki McThewson</b> / Retail Coordinator and Shop Mechanic</small>
           </div>
 
         </div>
@@ -106,7 +106,7 @@
           <!-- 50% -->
           <div class="col-b">
               <p>Do you think that the minimum wage law will have an impact on your life in some way?</p>
-              <div><blockquote><span>&ldquo;</span>Hopefully. I hope the minimum wage law also incentivizes companies to pay more even if they are already paying $15, just to maintain those pay structures [for other employees.]<span>&rdquo;</span></blockquote></div>
+              <div><blockquote><span>&ldquo;</span>Hopefully. I hope the minimum wage law also incentivizes companies to pay more even if they are already paying $15, just to maintain those pay structures.<span>&rdquo;</span></blockquote></div>
               <small><b>Lisa Ulrich</b> / Assistant Retail Manager </small>
           </div>
 
@@ -164,11 +164,11 @@
                   
                   <p>“While administrative data will give comprehensive results about changes in the number of jobs and hours worked, it doesn’t tell us how workers affected by the minimum wage view their quality of life,” explains Hilary Wething, a Ph.D. student at the Evans School and member of the Minimum Wage Study.</p>
                   <p>Wething, Hill and a team of other UW students make up a subgroup of the Minimum Wage Study aimed at collecting qualitative data related to the adoption of the new law. </p>
-                  <p>Over two years, their team conducted two waves of interviews with 55 local low-income workers. All 55 participants have children, and more than half are immigrants. At the start of the study, all were working for no more than $15 per hour. </p>
+                  <p>Over two years, their team conducted interviews with 55 local low-income workers, all of whom were working for no more than $15 per hour. All 55 participants have children, and more than half are immigrants. At the start of the study, all were working for no more than $15 per hour. </p>
                   <p>Beyond hours worked or tips received, the workers divulged more detailed and complex aspects of their lives. “We asked how well their work hours fit with their family life, what works about that and what doesn't,” explains Wething. “We asked questions that allowed them to provide a lot of detail that otherwise wouldn’t be given.”</p>
                   <p>Wething previously worked primarily with quantitative data on low-wage work, and was surprised by the intimate nature of qualitative research. </p>
                   <p>“Hearing people’s stories was not only moving, it also provided a lot of context about the perspective they bring to their work lives and how they view their roles as a parent,” she says. “It feels like you’re going from black and white to full color.”</p>
-                  <p>As the team embarks on the final wave of interviews this spring, Wething and Hill hope their data will help create a more complete picture in conjunction with the analysis of state administrative data and survey responses from employers. </p>
+                  <p>As the team embarks on final interviews this spring, Wething and Hill hope their data will help create a more complete picture in conjunction with the analysis of state administrative data and survey responses from employers. </p>
                   <p>“For instance, if we see that there were positive, negative, or no effects of the minimum wage change on workers, their earnings or their employment, then the interviews from the workers study could provide insight into why that’s the case,” says Hill.</p>
                   <p>For Wething, the opportunity to be part of the qualitative team has not only provided a one-of-a-kind research experience, it also helped her define her post-Ph.D. path. Inspired by collaborating with Evans School faculty, she hopes to follow in their footsteps and pursue a career in academia, which would enable her to continue researching the lives of low-wage workers and their families.</p>
                   <p>“To be involved with the Minimum Wage Study is a privilege,” she says. “People nationwide have eyes on what we’re doing. I feel inspired and grateful every day to get to work on this.”</p>
@@ -182,12 +182,12 @@
             <div class="row">
                 <div class="col-md-10 col-md-offset-1 copy reduced-pad">
                     
-                    <h2 class="slant"><span><span>Impact</span></span> abroad</h2>
+                    <h2 class="slant"><span><span>Global</span></span> impact</h2>
 
-                    <p class="bolder"><b>The Evans School Policy Analysis and Research Group (EPAR)</b></p>
+                    <p class="bolder"><b>The Evans School Policy Analysis &amp; Research Group (EPAR)</b></p>
 
-                    <p>The Evans School is a policy partner with impact far beyond the city of Seattle. By partnering with the Bill & Melinda Gates Foundation, students at the Evans School are able to reach and influence global communities.</p>
-                    <p>Reducing poverty around the world is graduate student Matthew Fowle’s dream. As a student researcher with EPAR, he’s looking for evidence-based solutions to the problem.</p>
+                    <p>The Evans School is a policy partner with impact far beyond the city of Seattle. By partnering with the Bill &amp; Melinda Gates Foundation, students at the Evans School are able to reach and influence global communities.</p>
+                    <p>Reducing poverty around the world is graduate student Matthew Fowle’s dream. As a student researcher with the Evans School Policy Analysis &amp; Research Group (EPAR), he's looking for evidence-based solutions to the problem.</p>
                     <p>Fowle, an M.P.A. candidate at the Evans School, was fairly young when he first decided to work for a more equitable world. He grew up in the English countryside. His mother was born in a small town in Southern Malaysia. He recalls travelling with her to visit his Malaysia relatives.</p>
                     <p>“I remember watching mothers boiling murky water to make it safe for their families to drink,” he says. “I started to question the reasons for poverty and to grapple with the inequity between my living conditions in England and the living conditions in my mother's hometown.” This led him to develop a commitment to work on behalf of people without access to safe water, shelter, adequate nutrition or formal education.</p>
                     <p> He went on to study evidence-based social intervention and policy evaluation at the University of Oxford, which intensified his commitment to equity. “I became determined to find evidence-based solutions that would more effectively reduce poverty.” </p>
@@ -214,9 +214,8 @@
                     <p>Led by Anderson and co-principal investigator Professor Travis Reynolds, ’11, an Evans School alumnus, EPAR is comprised of 15 graduate research assistants and a network of other UW faculty. The group's mission is to conduct objective research and analysis to help inform international stakeholders who tackle some of the world’s most complex and pressing financial, agricultural, public health and environmental challenges.</p>
 
 
-                    <div class="wide border"><img alt="Photo of Hilary Wething" src="<?php echo get_stylesheet_directory_uri() . '/immersive-stories/img/evans-school/matthew.jpg' ?>"><p><span><b>Hilary Wething</b> / Research Assistant / City of Seattle Minimum Wage Study</span></p></div>
-
-
+                    <div class="wide border"><img alt="Photo of Hilary Wething" src="<?php echo get_stylesheet_directory_uri() . '/immersive-stories/img/evans-school/matthew.jpg' ?>"><p><span><b>Matthew Fowle</b> / Research Assistant / Evans School Policy Analysis &amp; Research Group</span></p></div>
+                     
                     <p>To date, EPAR’s evidence reviews have been used by development practitioners, including those at the MasterCard Foundation, the International Telecommunications Union,, the Bill &amp; Melinda Gates Foundation, developing country government ministries and international non-governmental organizations. </p>
                     <p>One of Fowle’s EPAR research projects analyzes the use of digital financial services in developing countries. </p>
 
@@ -231,19 +230,20 @@
                   
                   <p>Through money transfer apps available on mobile phones, people can save, send and receive money, creating greater financial inclusion and — ideally — a pathway out of poverty.  </p>
                   
-                  <p>Fowle and his EPAR team members trawled through data from four South Asian and four Sub-Saharan countries to determine both why people adopt mobile money services and how they regularly use it. </p>
+                  <p>Fowle and his EPAR team members trawled through data from four South Asian and four Sub-Saharan African countries to determine both why people adopt mobile money services and how they regularly use it. </p>
                   
-                  <p>In just the eight years since EPAR has been in operation, Anderson has seen the immense benefit of the EPAR experience to students’ careers. “Combined with their Evans School professional training, this makes them phenomenal researchers. They learn great technical tools for finding, analyzing and communicating information; they learn from each other; and they push themselves beyond the threshold of what’s considered ‘quality’ work,” she says. “And they get hired very quickly after they graduate; it’s a problem for us every June — we lose these students to great places,” she adds with a laugh.</p>
+                  <p>In just the eight years since EPAR has been in operation, Anderson has seen the immense benefit of the EPAR experience to students’ careers. “Combined with their Evans School professional training, this makes them phenomenal policy researchers. They learn great technical tools for finding, analyzing and communicating information; they learn from each other; and they push themselves beyond the threshold of what's considered 'quality' work," she says. "They get hired very quickly after they graduate."</p>
 
                   <iframe width="1280" height="720" src="https://www.youtube.com/embed/QI50yL8rAIU?rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
-                  
-                  <p>EPAR alumni have taken up positions with USAID, the Center for Global Development, the Research Triangle Institute and PATH.</p>
+                  <small class="video-caption">Learn more about how the Evans School is impacting communities around the world through its partnership with the Bill &amp; Melinda Gates Foundation. </small>
+
+                  <p>EPAR alumni have taken up positions with global development organizations such as USAID, the Center for Global Development, the Research Triangle Institute and PATH.</p>
                   
                   <p>Fowle plans to earn his Ph.D. and continue to conduct original, innovative poverty reduction research that makes a difference in people’s lives, a decision influenced by his time at the Evans School.</p>
                   
                   <p>“It’s a rich and collaborative environment,” he says, “and a unique opportunity to conduct research with a dedicated team of talented colleagues and mentors who are renowned experts. Working with EPAR has enabled me to help tackle the complex equity problems that stirred my passions so strongly as a boy.”</p>
                   
-                  <p>Both Wething’s and Fowle’s extraordinary experiences have been fueled by the generosity of donors, whose gifts make student scholarships, fellowships and faculty professorships possible.</p>
+                  <p>Both Wething’s and Fowle’s extraordinary experiences have been fueled by the generosity of donors like you.  Your gifts make student scholarships, fellowships, and faculty professorships possible--and ensure that the Evans School's extraordinary real-world research will continue to make an impact.</p>
                   
                   <p>“Giving enables students to focus on being their best and brightest research selves,” says Wething. “I don’t have to worry about making ends meet. I can just be fully immersed in this process.”</p>
 
@@ -262,7 +262,7 @@
                                 <h2 class="headline-gold">Help make a difference</h2>
                                 <p>The University of Washington is undertaking its most ambitious campaign ever: Be Boundless — For Washington, For the World. By supporting the Evans School of Public Policy &amp; Governance, more students can take part in opportunities that change not only their lives, but also those of people around the world.</p>
                                 <div id="immersive-give-iframe"></div>
-                                <div class="boundless-button sm dark give"><span><a id="immersive-give" data-fund="SPHENF,GLOBPH" href="#">Give now</a></span></div>
+                                <div class="boundless-button sm dark give"><span><a id="immersive-give" data-fund="GSPAED,EVAN50" href="#">Give now</a></span></div>
                               </div>
                             </div>
                         </div>
