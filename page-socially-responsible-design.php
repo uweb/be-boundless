fc88149afd7d4b399cfc07f463d75bd468140900
Kilian <kilianf@uw.edu>
2017-06-16 11:46:35 -0700

    Added new CBE page

---
+++ page-socially-responsible-design.php
@@ -0,0 +1,304 @@
+<?php get_header( "campaign-open" ); ?>
+
+ <link rel='stylesheet' href='<?php echo get_stylesheet_directory_uri() . '/campaign/css/header.css' ?>' type='text/css' media='all' />
+ <link rel='stylesheet' href='<?php echo get_stylesheet_directory_uri() . '/immersive-stories/css/common.css' ?>' type='text/css' media='all' />
+ <link rel='stylesheet' href='<?php echo get_stylesheet_directory_uri() . '/immersive-stories/css/built-env.css' ?>' type='text/css' media='all' />
+
+<?php get_header( "campaign-close" ); ?>
+
+
+<div id="immersive-body">
+
+
+<section class="intro" id="intro-vid">          
+
+
+  <button id="pause">Pause</button>
+  <video  autoplay loop muted playsinline class="media" data-object-fit poster="<?php echo get_stylesheet_directory_uri() . '/immersive-stories/img/built-env/header.jpg' ?>" class="fullscreen-bg__video">
+      <!-- <source src="img/street-bw-webm.webm" type="video/webm"> -->
+      <source src="<?php echo get_stylesheet_directory_uri() . '/immersive-stories/video/built-env/header.mp4' ?>" type="video/mp4">
+      <!-- <source src="<?php echo plugins_url('uw-template-hierarchy') . '/assets/asb/video/public-health/header-new.webm' ?>" type="video/webm"> -->
+  </video>
+
+
+  <div class="intro-body">
+        <div class="row">
+          <div class="col-md-6 col-lg-4 col-md-offset-1 col-xs-10 col-lg-offset-2 col-xs-offset-1" id='intro-text'>
+              <h1>Garden <br>cities</h1>
+              <span class="udub-slant"><span></span></span>
+              <p>At the UW’s College of Built Environments, students like David de la Cruz, ’17, are partnering with some of the world’s most vulnerable communities in the fight for environmental justice.</p>
+              
+          </div>          
+          <div class="col-md-6 col-lg-3 col-md-offset-1 col-xs-12 col-lg-offset-1 col-xs-offset-1 play-button" >
+              <a data-lity href="https://www.youtube.com/embed/gKDK1RaLO78?autoplay=1&rel=0&showinfo=0">
+              <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                 width="137.909px" height="137.131px" viewBox="0 0 137.909 137.131" enable-background="new 0 0 137.909 137.131"
+                 xml:space="preserve">
+              <ellipse fill="none" stroke="#FFFFFF" stroke-width="7.8281" stroke-miterlimit="10" cx="68.399" cy="68.344" rx="57.675" ry="59.214"/>
+              <polygon fill="#FFFFFF" points="48.611,37.617 103.268,68.345 48.611,99.072 "/>
+              </svg>
+                <p>Play full video</p>
+              </a>
+          </div>
+      </div>
+    </div>
+
+
+  <section class="scrollit read-more">
+      <p>Scroll Down</p>
+      <div class="mouse">
+        <div class="scroll"></div>
+      </div>
+  </section>
+
+
+
+</section>
+
+
+
+<main id="main_content">
+
+
+      <section class="para p-1">
+        <h3>Where you live affects <span>how you live</span></h3>
+        <div class="cutout">
+            <div class="pic arrow-right"></div>
+            <div class="location">
+              <p>PUEBLO DEL RIO PUBLIC HOUSING PROJECTS</p>
+              <p>SOUTH CENTRAL LOS ANGELES</p>
+            </div>
+        </div>
+      </section>
+
+
+
+      <section class="text-block transparent la-river no-pad">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-10 col-md-offset-1 copy">
+                    <div class="row">
+                      <div class="col-md-8">
+
+<p>David de la Cruz has a question about power. “When we think about toxic sites and where they’re placed in relation to where people live, who’s left out of making those decisions?” </p>
+<p>“Often,” he answers, “it’s the people who live there. It’s low-income communities, working-class communities and communities of color who don’t have a say. They’re the ones who have to deal with the consequences of living close to a grain mill facility that constantly sees truck traffic, or living close to a yard that has trains passing back and forth.” </p>
+<p>And while these communities have some autonomy, he says, people are moving into areas that are already extensively polluted. There’s often no decision to be made — pollution is part of the package.</p>
+<p><i>Where</i> you live affects <i>how</i> you live. </p>
+
+                      </div>                 
+                       
+                    </div>
+                </div>
+          </div> 
+        </div>   
+      </section>
+      
+      <section class="para p-2">
+        <h3>A legacy of pollution  </h3>
+        <div class="cutout">
+            <div class="pic arrow-right"></div>
+            <div class="location">
+              <p>Cactus patch</p>
+              <p>South Central Los Angeles</p>
+            </div>
+        </div>
+      </section>
+
+
+      <section class="text-block transparent cactus no-pad">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-10 col-md-offset-1 copy">
+                    <div class="row">
+                      <div class="col-md-6">
+
+<p>De la Cruz, a 2017 graduate of the College of Built Environments’ master of landscape architecture program, grew up in Pueblo del Rio, a public housing project in South Central Los Angeles located just southwest of the Los Angeles River. </p>
+<p>The “garden city” complex — named for the open, grassy spaces that separate the rows of two-story buildings — went up in the early 1940s to support low-income, primarily black laborers who were flooding into the city to work in the defense industry in the wake of the Great Depression. South Central LA is the industrial center of the city, which has some of the most polluted air in the nation.</p>
+<p>Nearly 80 years later, the projects still house low-income black, Latino and Cambodian communities. And decades of inequity have left a void of opportunity that negatively affects the health, happiness and well-being of residents, who have limited ways to make change.</p>
+<p>But de la Cruz has a vision for change. Now, he’s returning from the UW to the community that raised him to partner with locals to help make the projects — and the world — a more environmentally equitable place for all. </p>
+<p>Creating a world where all people can live healthier and more fulfilling lives is also the mission of the UW’s <a href="http://uw.edu/populationhealth">Population Health Initiative</a>, which leverages capabilities and opportunities at the UW and beyond to advance well-being on a local and global scale. </p>
+
+
+                      </div>                 
+                      <div class="col-md-5 col-md-offset-1">
+                          <p class="bolder">The projects still house low-income black, Latino and Cambodian communities. And decades of inequity have left a void of opportunity that negatively affects the health, happiness and well-being of residents, who have limited ways to make change.</p>
+                      </div> 
+                    </div>
+                </div>
+          </div> 
+        </div>   
+      </section>
+
+      <section class="para p-3">
+        <h3>From the <br>ground up</h3>
+        <div class="cutout">
+            <div class="location">
+              <p>Gould Hall</p>
+              <p>University of Washington</p>
+            </div>
+        </div>
+      </section>
+
+      <section class="text-block transparent no-pad">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-10 col-md-offset-1 copy">
+                    <div class="row">
+                      <div class="col-md-6">
+
+<p>The spirit of what the College of Built Environments calls “emergent-convergent design” — community-driven projects that start small but contribute to a large-scale vision — pulses through the College. But it may run strongest where de la Cruz found his academic home: the Informal Urban Communities Initiative, a donor-supported design activism, research and education program cofounded by associate professor of landscape architecture Ben Spencer.</p>
+<p>“I was really interested in working with landscape architecture professors like Ben Spencer who have this emphasis on community engagement,” says de la Cruz, whose work was supported by the Margaret and Fritz Wagner Family Endowed Fellowship, the Norman J. Johnston Endowed Scholarship, and the University of Washington Graduate Opportunity Award.</p>
+<p>He was also inspired by his mother, a longtime community leader in Pueblo del Rio whose outreach has included setting up neighborhood food banks and hosting support groups for widows and women who have experienced domestic violence. “Seeing my mother’s commitment to this idea of the collective well-being of her community — and the faith her community has in her — has shaped the work that I do.”</p>
+
+                      </div>                 
+                      <div class="col-md-5 col-md-offset-1">
+                      <blockquote><span>&ldquo;</span>The College of Built Environments has a strong ethos of dedication to community-based development. Having colleagues, students and leadership who share the same philosophy of socially engaged, environmentally responsible design is a really important aspect of who we are.<span>&rdquo;</span></blockquote>
+                          <p class="attribute">Ben Spencer</p>
+                          <small>Associate Professor of Landscape Architecture</small>
+                      </div> 
+                    </div>
+                </div>
+          </div> 
+        </div>   
+      </section>
+
+
+      <section class="para p-4">
+        <h3>Green for the greater good</h3>
+        <div class="cutout">
+            <div class="pic arrow-right"></div>
+            <div class="location">
+              <p>Lomas de Zapallal</p>
+              <p>Peru</p>
+            </div>
+        </div>
+      </section>
+
+      
+      <section class="text-block transparent no-pad">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-10 col-md-offset-1 copy">
+                    <div class="row">
+                      <div class="col-md-6">
+
+<p>By deploying students like de la Cruz to work with residents on local improvement projects, the Informal Urban Communities Initiative aims to form cultural connections, then give some of the world’s most vulnerable communities a hand up. </p>
+<p>Among the most vulnerable populations are informal urban communities (commonly referred to as “slums”) on the outskirts of Lima, Peru, and Phnom Penh, Cambodia. Philanthropy-supported study-abroad opportunities enabled de la Cruz and his peers to spend extended time at each site, where they witnessed firsthand what environmental injustice looks like around the world — as well as the looming threats faced by underprivileged populations. </p>
+<p>These ranged from gentrification to displacement to toxic-emissions exposure. Some communities are plagued by drought, others by flooding. These historically oppressed groups have a feeling of being disposable, says de la Cruz. His goal? To work with communities to create longstanding solutions, together — an ethos core to the College of Built Environments. </p>
+<p>Through participatory design workshops, de la Cruz and the Informal Urban Communities Initiative partnered with residents, community leaders and local agencies as they worked to implement environments that better served the residents’ needs, whether that meant harvesting fog and constructing gardens to address a lack of water and food security in Lima or creating a safe, attractive green alleyway to enjoy in Phnom Penh. “My international experiences taught me how to build and cultivate working relationships with communities that have a wealth of information to tap into,” says de la Cruz. “Now I know what a successful workshop looks like and how to ensure that decisions are made in a way that benefits community members.” </p>
+
+                      </div>                 
+                      <div class="col-md-5 col-md-offset-1">
+                        <blockquote><span>&ldquo;</span>Pursuing a profession such as landscape architecture provides me and others who share a traditionally ‘other’ identity with the ability to pursue problem-solving in the name of environmental justice.<span>&rdquo;</span></blockquote>
+                        <p class="attribute">David de la Cruz, '17</p>
+                        <img alt="David de la Cruz" src="<?php echo get_stylesheet_directory_uri() . '/immersive-stories/img/built-env/CoBE_Immersive-2468.jpg' ?>" alt="">
+                      </div> 
+                    </div>
+                </div>
+          </div> 
+        </div>   
+      </section>
+
+
+
+      <section class="text-block transSection">
+
+          <div id="photo-1"></div>
+          <div id="photo-2"></div>
+      
+          <div class="text">
+            <div><p>He's returning</p></div>
+            <div><p id="fade-text">to the community that raised him</p></div>
+          </div>
+
+      </section>
+
+  
+      <section class="para p-5">
+        <h3>Envisioning a garden city</h3>
+        <div class="cutout">
+            <div class="pic arrow-right"></div>
+            <div class="location">
+              <p>Augustus F. Hawkins Nature Park</p>
+              <p>South Central Los Angeles</p>
+            </div>
+        </div>
+      </section>
+
+
+      <section class="text-block transparent no-pad">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-10 col-md-offset-1 copy">
+                    <div class="row">
+                      <div class="col-md-6">
+
+<p>“Emphasizing community engagement and the lived experiences of community members is how I expect to continue my role as a landscape architect,” says de la Cruz. “That’s what I’m interested in: community well-being through community collaboration.”</p>
+<p>As the 2017 National Olmstead Scholar — named by the premier national award program for landscape architecture students — he’ll have the chance to pursue this goal as he returns to South Central LA to address its most urgent threats: pollution, gentrification, displacement. (The restoration of the Los Angeles River, which involves all three, is high on his list.) He’s already applied his studies to the real world, hosting two workshops back in South Central LA that pulled in artists, park rangers, housing services coordinators and residents to mobilize de la Cruz’s community and imagine a neighborhood that trades industrial sites for green space. </p>
+<p>The work has started, says de la Cruz. “People have found ways to thrive in these environments regardless of the systemic issues that suppress them. Walk down a few aisles from here, and you can see all these plants that people grow,” he says from his mother’s kitchen table, motioning outside to the grassy lawns separating the rows of apartments. “They’ve found a way to connect to the soil that’s here, which the plants have the potential to clean up. Even though it’s not at a systemic level, people have found ways to have some sort of control over their own health through the way they intervene right in their front yards.”</p>
+<p>By applying this community knowledge on a larger scale, landscape architecture can combat widespread issues at the systemic level. “There are leaders in these communities,” says de la Cruz. “The intention is that they collaborate to take on projects at a front-yard level using local knowledge.” From there, he explains, they can scale up and shape healthier environments. </p>
+<p>“Through community involvement and participation, I’m hoping to work with residents to articulate how we can transform these larger infrastructures — ghosted railways, abandoned factories, the Los Angeles River — in a way that benefits them,” says de la Cruz. “So much knowledge already exists in these communities, and there are opportunities to have more landscape architects who come from diverse backgrounds. My mission isn’t to lead, but to help community members realize their leadership potential.” </p>
+
+                      </div>                 
+                      <div class="col-md-5 col-md-offset-1">
+                        <blockquote><span>&ldquo;</span>People lose their way a little bit because of the example that’s been set for them, but David is a different example. He shows kids that you can go away to school and be all you want to be, and that you can bring it back and pay it forward.<span>&rdquo;</span></blockquote>
+                        <p class="attribute">Carolynn Martin</p>
+                        <small>Executive Director of the Wellness Station </small>
+                        <img alt="David de la Cruz" src="<?php echo get_stylesheet_directory_uri() . '/immersive-stories/img/built-env/de-la-cruz-mom.jpg' ?>" alt="">
+                        <small style="margin-top: -25px">De la Cruz and his mother, Berta</small>
+                      </div> 
+                    </div>
+                </div>
+          </div> 
+        </div>   
+      </section>
+
+      <div class="clouds">
+        <img  class="cloud-img" src="<?php echo get_stylesheet_directory_uri() . '/immersive-stories/img/built-env/clouds.png' ?>" alt="">
+        <div class="mask"><img alt="David de la Cruz" src="<?php echo get_stylesheet_directory_uri() . '/immersive-stories/img/built-env/backyard.jpg' ?>" alt=""></div>
+        <div class="text">
+          <div><p>His mission isn't to lead</p></div>
+          <div><p id="fade-text">but to help community members realize their leadership potential</p></div>
+        </div>
+      </div>
+
+
+      <section class="text-block giving-widget branded arrow">
+          <div class="container">
+              <div class="row">
+                  <div class="col-md-10 col-md-offset-1 copy ">
+                    <div style="text-align: center">             
+                      <h2 class="headline-gold">What you care about <br>can change the world</h2>
+                      <p>The University of Washington is undertaking its most ambitious campaign ever: Be Boundless, For Washington, For the World. Join in the fight for environmental justice and healthier, more fulfilling futures for all by investing in students like David de la Cruz. </p>
+                      <div id="immersive-give-iframe"></div>
+                      <div class="boundless-button sm dark give"><span><a id="immersive-give" data-fund="AUPGEN,CBEIIN,LAPERU" href="#">Give now</a></span></div>
+                    </div>
+                  </div>
+              </div>
+        </div>
+      </section>
+
+
+      <!-- <div id="mapid"></div> -->
+
+
+</main>
+
+<div id="popup"></div>
+
+
+</div>
+
+
+ <script src="<?php echo get_stylesheet_directory_uri() . '/campaign/js/animationLibraries.min.js' ?>" type="text/javascript"></script>
+
+ <script src="<?php echo get_stylesheet_directory_uri() . '/immersive-stories/js/built-env.min.js' ?>" type="text/javascript"></script>
+ <script src="<?php echo get_stylesheet_directory_uri() . '/campaign/js/header.js' ?>" type="text/javascript"></script>
+
+ 
+
+ <?php get_template_part('footer', 'campaign'); ?>
+
+
+</body>
+</html>
