e4a1b738df610db2e4c0fe3d4a32299dbffe8924
Nick Winkelbauer <nwinkelb@uw.edu>
2017-07-11 16:29:25 -0700

    add video to cbe

--- page-socially-responsible-design.php
+++ page-socially-responsible-design.php
@@ -30,7 +30,7 @@
               
           </div>          
           <div class="col-md-6 col-lg-3 col-md-offset-1 col-xs-12 col-lg-offset-1 col-xs-offset-1 play-button" >
-              <a data-lity href="https://www.youtube.com/embed/gKDK1RaLO78?autoplay=1&rel=0&showinfo=0">
+              <a data-lity href="https://www.youtube.com/embed/Tr6KBzdaM0o?autoplay=1&rel=0&showinfo=0">
               <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
                  width="137.909px" height="137.131px" viewBox="0 0 137.909 137.131" enable-background="new 0 0 137.909 137.131"
                  xml:space="preserve">
