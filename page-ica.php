7c85fd7a678c1873df94b4578fe8bfe4ef4f54cd
Kilian <kilianf@uw.edu>
2017-07-19 11:57:17 -0700

    Added ICA story

---
+++ page-ica.php
@@ -0,0 +1,289 @@
+<?php get_header( "campaign-open" ); ?>
+
+ <link rel='stylesheet' href='<?php echo get_stylesheet_directory_uri() . '/campaign/css/header.css' ?>' type='text/css' media='all' />
+ <link rel='stylesheet' href='<?php echo get_stylesheet_directory_uri() . '/immersive-stories/css/common.css' ?>' type='text/css' media='all' />
+ <link rel='stylesheet' href='<?php echo get_stylesheet_directory_uri() . '/immersive-stories/css/ica.css' ?>' type='text/css' media='all' />
+
+<?php get_header( "campaign-close" ); ?>
+
+
+<div id="immersive-body">
+
+<section class="intro">          
+  <div class="pic-fade"></div>
+
+  <div class="intro-body">
+        <div class="row">
+          <div class="col-md-8 col-lg-6 col-md-offset-1 col-xs-10 col-lg-offset-2 col-xs-offset-1" id='intro-text'>
+              <h1><span>Havana McElvaine:</span> Changing the game</h1>
+              <span class="udub-slant"><span></span></span>
+              <p>A champion for racial equity and social justice, Husky women’s soccer standout Havana McElvaine, ’17, is exploring what it means to be black in America — and around the globe. </p>
+              
+          </div>          
+          <div class="col-md-4 col-lg-3 col-md-offset-1 col-xs-12 col-lg-offset-1 col-xs-offset-1 play-button" >
+          </div>
+      </div>
+    </div>
+
+
+  <section class="scrollit read-more">
+      <p>Scroll Down</p>
+      <div class="mouse">
+        <div class="scroll"></div>
+      </div>
+  </section>
+
+
+
+</section>
+
+
+
+<main id="main_content">
+
+
+
+
+      <section class="text-block transparent no-pad first-block">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 copy reduced-pad">
+                    <div class="row">
+                      <div class="col-md-6">
+
+                          <p>If you ask a group of six-year-olds what they want to be when they grow up, there’s a good chance most of them will say “athlete.” In reality, the vast majority of those kids will wind up doing something else — but Havana McElvaine was determined to beat the odds. </p>
+                          <p>It started when she joined the local rec soccer team, the Pink Flamingos, because all her friends had. They kicked the ball around the park every weekend, playing until the golden summer sun dipped below the horizon. “I loved it,” says the Denver native. </p>
+                          <p>Soon the game turned into more than a hobby — it was her life. “By the time I was 13 or 14, I said, ‘OK, I’m really going to do this,’” she says. By 15, she was getting calls from the UW.</p>
+                          <p>The UW was one of the first universities that recruited McElvaine, who earned her bachelor’s in sociology in 2017. “I remember sitting in Coach Gallimore’s office when I was a junior in high school and hearing her offer me a scholarship,” she says. “My mom and I just looked at each other like, ‘What? That’s huge.’ Someone cared enough about me doing something I loved that they were willing to support it so I could do it without any financial burden, and that’s a massive privilege.” </p>
+                          <p>For McElvaine, soccer — not school — had always been her sole identity. When she became a Husky, that all changed. </p>
+
+                      </div>       
+                      <div class="col-md-5 col-md-offset-1">
+
+                          <div class="featured-full arrow-right">
+                            <img alt="" src="<?php echo get_stylesheet_directory_uri() . '/immersive-stories/img/ica/players.jpg' ?>">
+                          </div>
+
+                      </div>                 
+                       
+                    </div>
+                </div>
+          </div> 
+        </div>   
+      </section>
+      
+      <iframe width="1280" height="720" src="https://www.youtube.com/embed/Mz-NPAq924g?rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
+
+ 
+
+
+      <section class="para arrow-top p-2">
+        <h3><span>The academic <i>and the activist</i></span></h3>
+        <div class="cutout">
+            <div class="pic arrow-bottom"></div>
+<!--             <div class="location">
+              <p>Cactus patch</p>
+              <p>South Central Los Angeles</p>
+            </div> -->
+        </div>
+      </section>
+
+
+      <section class="text-block transparent no-pad">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-10 col-md-offset-1 copy">
+                    <div class="row">
+                      <div class="col-md-6">
+
+<p>“The first time I saw myself as someone who could be good at soccer and scholarly was when I got to the UW,” says McElvaine, a four-year defender for the Huskies. “The culture of my team and of the entire athletic department is that it’s cool and important to do well in school. My athletic experience was only as valuable as how I supplemented it with my academics, and vice versa.”</p>
+
+<p>As an incoming freshman, McElvaine wasn’t set on a major. She knew she liked talking about race and equity and social justice, but when she enrolled in Professor Alexes Harris’ course “Race and Ethnicity in the United States,” she discovered that not only could she study these topics that were so personal to her, but there were other women of color already doing it.</p>
+
+<p>“That was the first time I’d ever had a professor who looked like me, and that was transformative,” she says. “Her class is what sparked my interest in sociology and looking at these problems in a really scientific, pragmatic, critical way.” </p>
+
+                      </div>                 
+                      <div class="col-md-5 col-md-offset-1">
+                      <blockquote><span>&ldquo;</span>Havana is committed to and passionate about justice. She’s one of those students I think most professors wish they had in their class. She makes you think about the question you just asked, and think about it differently.<span>&rdquo;</span></blockquote>
+                          <p class="attribute"> Alexes Harris, Professor of Sociology </p>
+                      </div>
+                    </div>
+                </div>
+          </div> 
+        </div>   
+      </section>
+
+      <section class="text-block transSection">
+
+          <div id="photo-1"></div>
+          <div id="photo-2"></div>
+      
+          <p><span>McElvaine and her mentors (from left): Lesle Gallimore, Head Coach, Husky Women’s Soccer; Dorothy Bullitt, Senior Lecturer, Evans School; Alexes Harris, Professor, Department of Sociology</span></p>
+
+      </section>
+
+
+
+      <section class="para arrow-top p-3">
+        <h3><span>Social justice <i>takes center field</i></span></h3>
+        <div class="cutout">
+            <div class="pic arrow-bottom"></div>
+<!--             <div class="location">
+              <p>Gould Hall</p>
+              <p>University of Washington</p>
+            </div> -->
+        </div>
+      </section>
+
+      <section class="text-block transparent no-pad">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-10 col-md-offset-1 copy">
+                    <div class="row">
+                      <div class="col-md-6">
+
+<p>On Sept. 23, 2016 — minutes before the Husky women’s soccer team kicked off its season conference opener — McElvaine dropped a bare knee on the damp playing field of Husky Soccer Stadium as “The Star-Spangled Banner” rang through the loudspeakers. </p>
+<p>The moment was inspired by San Francisco 49ers then-quarterback Colin Kaepernick, who had opted one month earlier not to stand during the pregame national anthem. Instead he sat on the bench, arms crossed. It was a quiet response to a polarizing issue: police brutality against people of color. </p>
+<p>A movement took hold as athletes across the country sat, kneeled, raised fists and linked arms for their own causes, sparking a nationwide conversation about human rights. </p>
+<p>The day before she took a knee, McElvaine had watched footage of the shooting of Terence Crutcher, a black man killed by a police officer in Tulsa, Oklahoma. “I’d seen a bunch of videos before,” she says, “but with this one, I just couldn’t get myself together. I couldn’t control my crying.”</p>
+<p>At practice, McElvaine powered through her drills, but her coaches and teammates knew something was wrong. </p>
+<p>“The next day, before the national anthem, Coach Gallimore looked at me and said, ‘Havana, if you want to kneel, you can,’” recalls McElvaine. “I hadn’t even thought about kneeling, but in that moment, she really knew me better than I knew myself. She enabled me to be in solidarity with a movement I cared about, and that was really powerful for me.” </p>
+
+
+                      </div>                 
+                      <div class="col-md-5 col-md-offset-1">
+                        <blockquote><span>&ldquo;</span>I think to create connection, healing and togetherness, you need an ability to empathize with people who are different from you.<span>&rdquo;</span></blockquote>
+                          <p class="attribute"> Havana McElvaine, ‘17</p>
+                        <div class="featured-full arrow-right">
+                          <img alt="" src="<?php echo get_stylesheet_directory_uri() . '/immersive-stories/img/ica/students.jpg' ?>">
+                        </div>
+
+                      </div> 
+                    </div>
+                </div>
+          </div> 
+        </div>   
+      </section>
+
+
+      <section class="para arrow-top p-4">
+        <h3><span>Sparking a <i>community conversation</i></span></h3>
+        <div class="cutout">
+            <div class="pic arrow-bottom"></div>
+<!--             <div class="location">
+              <p>Lomas de Zapallal</p>
+              <p>Peru</p>
+            </div> -->
+        </div>
+      </section>
+
+      
+      <section class="text-block transparent no-pad">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-10 col-md-offset-1 copy">
+                    <div class="row">
+                      <div class="col-md-6">
+
+<p>A major priority supported by the UW’s Race &amp; Equity Initiative, the movement for racial equity and social justice was also top of mind for McElvaine’s teammates and other student-athletes. “It became apparent that we needed to initiate these uncomfortable conversations and build a community around these topics,” she says. </p>
+<p>So her team created a new culture where they opened up the floor and talked about everything, looping in coaches, administrators and academic advisers. “It was this really poignant time for all of us to be student-athletes and to have this platform and this space to talk,” says McElvaine, who stepped into a leadership role as a senior and team captain. </p>
+<p>But the poignancy ran even deeper for McElvaine. In her 21 years, she’d always seen her identities as separate: On the field, she was a soccer player. In the classroom, a student. In her community, a champion for change. Now she was everything at once.</p>
+<p>During her four years at the UW, McElvaine learned about education and policy reform and public housing. She helped a professor track how children’s perceptions of race and identity change over time. Then, for her senior honors thesis, she created her own research project, analyzing the impact that repeated video exposure to police violence and shootings has on black male college students.  </p>
+<p>In a series of focus groups with black men on campus, McElvaine asked them what it meant to see these incredibly violent incidents happening in real time — over and over again. “The conversations were heavy,” says McElvaine, “but so important.” </p>
+
+
+                      </div>                 
+                      <div class="col-md-5 col-md-offset-1">
+                        <blockquote><span>&ldquo;</span>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus suscipit ac turpis et mollis. Nullam iaculis vitae purus tincidunt ultrices. <span>&rdquo;</span></blockquote>
+                        <p class="attribute">Quote from coach Gallimore</p>
+                      </div> 
+                    </div>
+                </div>
+          </div> 
+        </div>   
+      </section>
+
+
+
+
+  
+      <section class="para arrow-top p-5">
+        <h3><span>The opportunity <i>of a lifetime</i></span></h3>
+        <div class="cutout">
+            <div class="pic arrow-bottom"></div>
+<!--             <div class="location">
+              <p>Augustus F. Hawkins Nature Park</p>
+              <p>South Central Los Angeles</p>
+            </div> -->
+        </div>
+      </section>
+
+
+      <section class="text-block transparent no-pad">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-10 col-md-offset-1 copy">
+                    <div class="row">
+                      <div class="col-md-6">
+
+<p>It wasn’t until she attended the 2017 Black Student-Athlete Summit in Austin, Texas, that McElvaine realized she didn’t have a deep-rooted knowledge of the history of racial identity, despite spending every day talking and thinking about it. “There was this huge disconnect,” she says. “I have this really narrow understanding of black history not only in the U.S., but globally — the history of where I came from and why I’m here.” </p>
+<p>Understanding black history became the inspiration for her Bonderman Fellowship: an eight-month donor-supported international journey designed to enable students to “explore, be open to the unexpected and come to know the world in new ways.” </p>
+<p>In January 2018, McElvaine will embark on a solo nine-country trip spanning North America, South America, Africa and Asia, tracing the African diaspora and the transatlantic slave trade as she connects with people around the world. </p>
+<p>“I want to use this experience to immerse myself and learn all I can about black history globally,” says McElvaine, the first Husky student-athlete to be awarded the prestigious fellowship. “I want to answer questions like, ‘Is my experience here as a black woman specific to America? Is it specific to me? Is it a universal experience? How do black women around the world think about race and equity and how those things interact with gender and activism and social justice?’”</p>
+<p>Then she hopes to bring what she’s learned back home.  </p>
+<p>“I’m most excited to see how other people are finding ways to build community and connect with each other, in spite of all the challenges happening around the world. Whatever work I go into, whether that’s in athletics or some other sphere, I’m excited to inspire change and fight for social justice.” </p>
+
+
+                      </div>                 
+                      <div class="col-md-5 col-md-offset-1">
+                        <div class="featured-full arrow-right">
+                          <img alt="" src="<?php echo get_stylesheet_directory_uri() . '/immersive-stories/img/ica/havana.jpg' ?>">
+                        </div>
+                      </div> 
+                    </div>
+                </div>
+          </div> 
+        </div>   
+      </section>
+
+
+
+      <section class="text-block giving-widget branded arrow">
+          <div class="container">
+              <div class="row">
+                  <div class="col-md-10 col-md-offset-1 copy ">
+                    <div style="text-align: center">             
+                      <h2 class="headline-gold">What you care about <br>can change the world</h2>
+                      <p>The University of Washington is undertaking its most ambitious campaign ever: Be Boundless, For Washington, For the World. By supporting Husky Athletics, you can transform the Husky Experience and help more student-athletes like Havana McElvaine pursue their passions.</p>
+                      <div id="immersive-give-iframe"></div>
+                      <div class="boundless-button sm dark give"><span><a id="immersive-give" data-fund="AUPGEN,CBEIIN,LAPERU" href="#">Give now</a></span></div>
+                    </div>
+                  </div>
+              </div>
+        </div>
+      </section>
+
+
+      <!-- <div id="mapid"></div> -->
+
+
+</main>
+
+<div id="popup"></div>
+
+
+</div>
+
+
+ <script src="<?php echo get_stylesheet_directory_uri() . '/campaign/js/animationLibraries.min.js' ?>" type="text/javascript"></script>
+
+ <script src="<?php echo get_stylesheet_directory_uri() . '/immersive-stories/js/ica.min.js' ?>" type="text/javascript"></script>
+ <script src="<?php echo get_stylesheet_directory_uri() . '/campaign/js/header.js' ?>" type="text/javascript"></script>
+
+ 
+
+ <?php get_template_part('footer', 'campaign'); ?>
+
+
+</body>
+</html>
