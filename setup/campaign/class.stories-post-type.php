7db406ec63bea575013c636954eeb155b88758f6
Nick Winkelbauer <nwinkelb@uw.edu>
2017-08-14 15:06:09 -0700

    Campaign : added shortcode for stories

--- setup/campaign/class.stories-post-type.php
+++ setup/campaign/class.stories-post-type.php
@@ -56,13 +56,48 @@ if ( ! post_type_exists( 'stories' ) ):
       'default' => null
     ), $atts);
 
+    $return = '<section id="impact-stories">' .
+                '<h2>Stories of impact</h2>' .
+                '<hr>' .
+                '<div class="container">';
+
     //get all stories post type
     //filter out those with exclude checkmark
     //sort by order
+    $args = array(
+        'post_type' => 'stories',
+        'nopaging'  => true,
+        'meta_key'  => 'exclude',              //check exclude checkbox
+        'meta_value' => !'on',                //test = not on
+        'order'     => 'ASC',
+        'orderby'   => 'menu_order',
+    );              
+    $the_query = new WP_Query( $args );
+    $count = 0;
+    if ( $the_query->have_posts() ) :
+        while ( $the_query->have_posts() ) : $the_query->the_post();
+                $return .= '<div class="story">' . 
+                                '<h3>' . get_the_title() . '</h3>' .
+                                '<div class="image-equalizer" style="background-image: ' . wp_get_attachment_image_src( get_post_thumbnail_id() , array(300,200))[0] . ';"></div>' .
+                                '<hr>' .
+                                '<p class="subheader">' . get_the_content() . '</p>' .
+                            '</div>';
+        endwhile;
+    endif;
+    // Reset Post Data
+    wp_reset_postdata();
+    
+
     //print in sections of 3 ie class= section1/2/3/4...
     //put section current in middle, current-1 in left outside page, current+1 outside on right, all else hidden
-    
-    $return = '';
+    $return .= '</div>' .
+                '<a class="left stories-control" href="" data-slide="prev">' .
+                    '<span></span>' .
+                '</a>' .
+                '<a class="right stories-control" href="" data-slide="next">' .
+                    '<span></span>' .
+                '</a>' .
+              '</section>';
 
     return $return;
 
