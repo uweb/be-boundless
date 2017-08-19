6b61302ca0681eee7eeed2c4f01feff89e32ff11
Nick Winkelbauer <nwinkelb@uw.edu>
2017-08-18 19:30:13 -0700

    adding in function files one by one to troubleshoot

--- setup/class.campaign-functions.php
+++ setup/class.campaign-functions.php
@@ -2,14 +2,14 @@
 //New header for giving with editable fields
 //require 'campaign/class.header-giving.php';
 //Home slide shortcode
-require 'campaign/class.home-slide-shortcode.php';
+//require 'campaign/class.home-slide-shortcode.php';
 //side by side img text post type & shortcode
-require 'campaign/class.excerpt-post-type.php';
+//require 'campaign/class.excerpt-post-type.php';
 //fast facts post type and shortcode
-require 'campaign/class.fast-fact-post-type.php';
+//require 'campaign/class.fast-fact-post-type.php';
 //giving shortcode
-require 'campaign/class.giving-module-shortcode.php';
+//require 'campaign/class.giving-module-shortcode.php';
 //stories of impact shortcode and additional archive controls
-require 'campaign/class.stories-post-type.php';
-require 'campaign/class.story-page-attributes-meta-box.php';
+//require 'campaign/class.stories-post-type.php';
+//require 'campaign/class.story-page-attributes-meta-box.php';
 ?>
