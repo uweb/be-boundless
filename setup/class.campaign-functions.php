edbb4d1a3ddd76d9b91d3e8f80630e8e392c5158
Nick Winkelbauer <nwinkelb@uw.edu>
2017-08-18 19:33:18 -0700

    adding in function files one by one to troubleshoot

--- setup/class.campaign-functions.php
+++ setup/class.campaign-functions.php
@@ -6,7 +6,7 @@
 //side by side img text post type & shortcode
 require 'campaign/class.excerpt-post-type.php';
 //fast facts post type and shortcode
-//require 'campaign/class.fast-fact-post-type.php';
+require 'campaign/class.fast-fact-post-type.php';
 //giving shortcode
 //require 'campaign/class.giving-module-shortcode.php';
 //stories of impact shortcode and additional archive controls
