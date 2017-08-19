892b46b6166b8f846cda4fc5bcd62f620bbec70f
Nick Winkelbauer <nwinkelb@uw.edu>
2017-08-18 20:08:09 -0700

    adding in function files one by one to troubleshoot

--- setup/campaign/class.stories-post-type.php
+++ setup/campaign/class.stories-post-type.php
@@ -55,73 +55,73 @@ if ( ! post_type_exists( 'stories' ) ):
         }
     }
 
-  function stories_shortcode( $atts )
-  {
+  // function stories_shortcode( $atts )
+  // {
 
-    $atts = shortcode_atts( array(
-      'default' => null
-    ), $atts);
+  //   $atts = shortcode_atts( array(
+  //     'default' => null
+  //   ), $atts);
 
-    $return = '<section id="impact-stories">' .
-                '<div class="content">' . 
-                    '<h2>Stories of impact</h2>' .
-                    '<hr class="no-margin">' .
-                '</div>' .
-                '<div class="container"><div class="story-container"><div id="story-slider">';
+  //   $return = '<section id="impact-stories">' .
+  //               '<div class="content">' . 
+  //                   '<h2>Stories of impact</h2>' .
+  //                   '<hr class="no-margin">' .
+  //               '</div>' .
+  //               '<div class="container"><div class="story-container"><div id="story-slider">';
 
-    //get all stories post type
-    //filter out those with exclude checkmark
-    //sort by order
-    $args = array(
-        'post_type' => 'stories',
-        'nopaging'  => true,
-        'meta_key'  => 'exclude',              //check exclude checkbox
-        'meta_value' => !'on',                //test = not on
-        'order'     => 'ASC',
-        'orderby'   => 'menu_order',
-    );              
-    $the_query = new WP_Query( $args );
-    $count = 0;
-    if ( $the_query->have_posts() ) :
-        while ( $the_query->have_posts() ) : $the_query->the_post();
-                $return .= '<div class="story"><a class="story-link" href="' . get_post_meta( get_the_ID(), 'url', true ) . '">' . 
-                                '<h3>' . get_the_title() . '</h3>' .
-                                '<div class="image-equalizer" style="background-image: url(' . wp_get_attachment_image_src( get_post_thumbnail_id() , array(300,200))[0] . ');"></div>' .
-                                '<hr>' .
-                                '<p class="subheader">' . get_the_content() . '</p>' .
-                            '</a></div>';
-        endwhile;
-    endif;
-    // Reset Post Data
-    wp_reset_postdata();
+  //   //get all stories post type
+  //   //filter out those with exclude checkmark
+  //   //sort by order
+  //   $args = array(
+  //       'post_type' => 'stories',
+  //       'nopaging'  => true,
+  //       'meta_key'  => 'exclude',              //check exclude checkbox
+  //       'meta_value' => !'on',                //test = not on
+  //       'order'     => 'ASC',
+  //       'orderby'   => 'menu_order',
+  //   );              
+  //   $the_query = new WP_Query( $args );
+  //   $count = 0;
+  //   if ( $the_query->have_posts() ) :
+  //       while ( $the_query->have_posts() ) : $the_query->the_post();
+  //               $return .= '<div class="story"><a class="story-link" href="' . get_post_meta( get_the_ID(), 'url', true ) . '">' . 
+  //                               '<h3>' . get_the_title() . '</h3>' .
+  //                               '<div class="image-equalizer" style="background-image: url(' . wp_get_attachment_image_src( get_post_thumbnail_id() , array(300,200))[0] . ');"></div>' .
+  //                               '<hr>' .
+  //                               '<p class="subheader">' . get_the_content() . '</p>' .
+  //                           '</a></div>';
+  //       endwhile;
+  //   endif;
+  //   // Reset Post Data
+  //   wp_reset_postdata();
     
 
-    //print in sections of 3 ie class= section1/2/3/4...
-    //put section current in middle, current-1 in left outside page, current+1 outside on right, all else hidden
-    $return .= '</div></div>' .
-                '<button class="left stories-control prevSlide">' .
-                    '<span>' .
-                        '<svg aria-hidden="true" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="612px" height="792px" viewBox="0 0 612 792" enable-background="new 0 0 46.25 101.1" xml:space="preserve" id="arrow1" transform="scale(5)">' .
-                            '<path fill-rule="evenodd" clip-rule="evenodd" fill="#4B2E83" d="M286.246,360.073c0.591-0.632,1.233-1.36,1.918-2.046 c14.945-14.954,29.963-29.837,44.775-44.92c2.466-2.511,3.853-1.065,4.906,0.663c1.032,1.694,5.375,2.399,1.713,5.967 c-12.398,12.082-24.47,24.498-36.849,36.601c-2.804,2.741-2.906,4.401-0.043,7.189c12.118,11.805,23.883,23.973,36.056,35.717 c4.803,4.635-0.516,5.568-2.147,8.044c-2.629,3.985-3.7,0.151-4.901-1.039c-14.379-14.242-28.67-28.572-42.966-42.897 C287.803,362.444,286.464,361.839,286.246,360.073z"/>' .
-                        '</svg>' . 
-                    '</span>' .
-                '</button>' .
-                '<button class="right stories-control nextSlide">' .
-                    '<span>' .
-                        '<svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="46.25px" height="101.1px" viewBox="0 0 612 792" enable-background="new 0 0 46.25 101.1" xml:space="preserve" id="arrow2" transform="scale(5)">' . 
-                            '<path xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd" fill="#4B2E83" d="M341,360.739c-0.591,0.632-1.233,1.36-1.918,2.046   c-14.945,14.954-29.963,29.837-44.775,44.92c-2.466,2.511-3.853,1.065-4.906-0.663c-1.032-1.694-5.375-2.399-1.713-5.967   c12.398-12.082,24.47-24.498,36.849-36.601c2.804-2.741,2.906-4.401,0.043-7.189c-12.118-11.805-23.883-23.973-36.056-35.717   c-4.803-4.635,0.516-5.568,2.148-8.044c2.629-3.985,3.7-0.151,4.901,1.039c14.379,14.242,28.67,28.572,42.966,42.897   C339.443,358.368,340.782,358.974,341,360.739z"/>' .
-                        '</svg>' . 
-                    '</span>' .
-                '</button>' .
-              '</div></section>';
+  //   //print in sections of 3 ie class= section1/2/3/4...
+  //   //put section current in middle, current-1 in left outside page, current+1 outside on right, all else hidden
+  //   $return .= '</div></div>' .
+  //               '<button class="left stories-control prevSlide">' .
+  //                   '<span>' .
+  //                       '<svg aria-hidden="true" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="612px" height="792px" viewBox="0 0 612 792" enable-background="new 0 0 46.25 101.1" xml:space="preserve" id="arrow1" transform="scale(5)">' .
+  //                           '<path fill-rule="evenodd" clip-rule="evenodd" fill="#4B2E83" d="M286.246,360.073c0.591-0.632,1.233-1.36,1.918-2.046 c14.945-14.954,29.963-29.837,44.775-44.92c2.466-2.511,3.853-1.065,4.906,0.663c1.032,1.694,5.375,2.399,1.713,5.967 c-12.398,12.082-24.47,24.498-36.849,36.601c-2.804,2.741-2.906,4.401-0.043,7.189c12.118,11.805,23.883,23.973,36.056,35.717 c4.803,4.635-0.516,5.568-2.147,8.044c-2.629,3.985-3.7,0.151-4.901-1.039c-14.379-14.242-28.67-28.572-42.966-42.897 C287.803,362.444,286.464,361.839,286.246,360.073z"/>' .
+  //                       '</svg>' . 
+  //                   '</span>' .
+  //               '</button>' .
+  //               '<button class="right stories-control nextSlide">' .
+  //                   '<span>' .
+  //                       '<svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="46.25px" height="101.1px" viewBox="0 0 612 792" enable-background="new 0 0 46.25 101.1" xml:space="preserve" id="arrow2" transform="scale(5)">' . 
+  //                           '<path xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd" fill="#4B2E83" d="M341,360.739c-0.591,0.632-1.233,1.36-1.918,2.046   c-14.945,14.954-29.963,29.837-44.775,44.92c-2.466,2.511-3.853,1.065-4.906-0.663c-1.032-1.694-5.375-2.399-1.713-5.967   c12.398-12.082,24.47-24.498,36.849-36.601c2.804-2.741,2.906-4.401,0.043-7.189c-12.118-11.805-23.883-23.973-36.056-35.717   c-4.803-4.635,0.516-5.568,2.148-8.044c2.629-3.985,3.7-0.151,4.901,1.039c14.379,14.242,28.67,28.572,42.966,42.897   C339.443,358.368,340.782,358.974,341,360.739z"/>' .
+  //                       '</svg>' . 
+  //                   '</span>' .
+  //               '</button>' .
+  //             '</div></section>';
 
-    return $return;
+  //   return $return;
 
     
 
 
-  }
-  add_shortcode( 'stories', 'stories_shortcode' );
+  // }
+  // add_shortcode( 'stories', 'stories_shortcode' );
 
     
 endif;
