3c6fc8e8d6a00d390bbc024b3f7aa6c96231f423
Nick Winkelbauer <nwinkelb@uw.edu>
2017-09-05 09:03:52 -0700

    Fixed header JS for non-updated pages

--- campaign/js/header.js
+++ campaign/js/header.js
@@ -1,8 +1,9 @@
 // Mobile check
-
-// var isMobile = false,
-// 	userClosedMenu = false;
-
+// 
+/* V1 */
+var isMobile = false,
+	userClosedMenu = false;
+/* end V1 */
 
 $(function(){
 
@@ -26,50 +27,77 @@ $(function(){
 			$(".cv2-header-grey").slideToggle("slow");
 		});
 
-	// var bodyEl 		= document.getElementsByTagName('body')[0].classList,
-	// 	campExpand 	= document.getElementById('campaign-expand'),
-	// 	thin		= document.getElementsByClassName('thin')[0],
-	// 	thick		= document.getElementsByClassName('thick')[0],
-	 	// give 		= document.getElementById('give-now')
-	 	var give 		= document.getElementById('cv2-give'),
-	 		close 		= document.getElementById('cv2-close');
+/* V1 */
+	var bodyEl 		= document.getElementsByTagName('body')[0].classList,
+		campExpand 	= document.getElementById('campaign-expand'),
+		thin		= document.getElementsByClassName('thin')[0],
+		thick		= document.getElementsByClassName('thick')[0],
+	 	give 		= document.getElementById('give-now'), /* end V1 */
+	 	giveNew		= document.getElementById('cv2-give'),
+	 	close 		= document.getElementById('cv2-close');
 
-	// document.getElementById('campaign-expand').addEventListener('click', function(){
-	// 	bodyEl.toggle('active-header')
-	// 	// Register user's preference for menu
-	// 	userClosedMenu = (userClosedMenu === true) ? false : true;
-	// 	if (userClosedMenu) {
-	// 		thin.setAttribute('aria-hidden', 'false');
-	// 		thick.setAttribute('aria-hidden', 'true');
-	// 		// Give animation 410ms to play
-	// 		setTimeout(function(){
-	// 			thin.focus();
-	// 		}, 410)
-	// 	} else {
-	// 		thin.setAttribute('aria-hidden', 'true');
-	// 		thick.setAttribute('aria-hidden', 'false');
-	// 		setTimeout(function(){
-	// 			thick.focus();
-	// 		}, 410)
-	// 	}
+/* V1 */
+	document.getElementById('campaign-expand').addEventListener('click', function(){
+		bodyEl.toggle('active-header')
+		// Register user's preference for menu
+		userClosedMenu = (userClosedMenu === true) ? false : true;
+		if (userClosedMenu) {
+			thin.setAttribute('aria-hidden', 'false');
+			thick.setAttribute('aria-hidden', 'true');
+			// Give animation 410ms to play
+			setTimeout(function(){
+				thin.focus();
+			}, 410)
+		} else {
+			thin.setAttribute('aria-hidden', 'true');
+			thick.setAttribute('aria-hidden', 'false');
+			setTimeout(function(){
+				thick.focus();
+			}, 410)
+		}
 
-	// });
+	});
 
-	// isMobile = window.matchMedia("only screen and (max-width: 768px)");
+	isMobile = window.matchMedia("only screen and (max-width: 768px)");
 
 
 	// Toggle menu open on desktop
-	// if(!isMobile.matches) {
-	//  	bodyEl.toggle('active-header');
-	//  	campExpand.setAttribute('aria-hidden', 'true');
-	// }
+	if(!isMobile.matches) {
+	 	bodyEl.toggle('active-header');
+	 	campExpand.setAttribute('aria-hidden', 'true');
+	}
 
 	// Check for empty
-	// function isEmpty( el ){
-	//   return !$.trim(el.html())
-	// }
+	function isEmpty( el ){
+	  return !$.trim(el.html())
+	}
 
 	give.addEventListener('click', function(e){
+
+		e.preventDefault();
+
+		if(isEmpty($('#give-iframe'))) {
+			$('<iframe>', {
+				src: 'https://online.gifts.washington.edu/secure/?nobanner=true&activateTab=0&appeal=17XBS',
+				frameborder: 0,
+				width: '100%',
+				height: '100%',
+			}).appendTo('#give-iframe');
+		}
+
+		if(give.innerHTML === 'Give now'){
+			give.innerHTML = 'Close';
+		} else {
+			give.innerHTML = 'Give now';
+		}
+
+		$('body').toggleClass('give-modal-active');
+
+	})
+
+/* end V1 */
+
+	giveNew.addEventListener('click', function(e){
 
 		e.preventDefault();
 
@@ -84,11 +112,13 @@ $(function(){
 			}).appendTo('#give-iframe');
 		}
 
+		/* V1 */
 		// if(give.innerHTML === 'Give now'){
 		// 	give.innerHTML = 'Close';
 		// } else {
 		// 	give.innerHTML = 'Give now';
 		// }
+		/* end V1 */
 
 		$('body').toggleClass('give-modal-active');
 
