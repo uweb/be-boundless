5b2f3410ee39a7f6d76c9ab13835c83d103f001b
Nick Winkelbauer <nwinkelb@uw.edu>
2017-09-08 11:18:37 -0700

    lengthened darken on enviro

--- immersive-stories/js/environment.js
+++ immersive-stories/js/environment.js
@@ -177,7 +177,7 @@ new ScrollMagic.Scene({triggerElement: "#predators", duration: sectionHeight, tr
           .setPin("#predators > .enviro-bg")
           .addTo(currentController);
 
-new ScrollMagic.Scene({triggerElement: "#pred-content", duration: (0.7*sectionHeight), triggerHook:0.5})
+new ScrollMagic.Scene({triggerElement: "#pred-content", duration: (0.8*sectionHeight), triggerHook:0.7})
           .setClassToggle(".enviro-bg", "darken")
           .addTo(currentController);
 
@@ -186,7 +186,7 @@ new ScrollMagic.Scene({triggerElement: "#scavengers", duration: scavHeight, trig
           .setPin("#scavengers > .enviro-bg")
           .addTo(currentController);
 
-new ScrollMagic.Scene({triggerElement: "#scav-content", duration: (0.7*scavHeight), triggerHook:0.5})
+new ScrollMagic.Scene({triggerElement: "#scav-content", duration: (0.8*scavHeight), triggerHook:0.7})
           .setClassToggle(".enviro-bg", "darken")
           .addTo(currentController);
 
@@ -195,7 +195,7 @@ new ScrollMagic.Scene({triggerElement: "#prey", duration: preyHeight, triggerHoo
           .setPin("#prey > .enviro-bg")
           .addTo(currentController);
 
-new ScrollMagic.Scene({triggerElement: "#prey-content", duration: (0.7*preyHeight), triggerHook:0.5})
+new ScrollMagic.Scene({triggerElement: "#prey-content", duration: (0.8*preyHeight), triggerHook:0.7})
           .setClassToggle(".enviro-bg", "darken")
           .addTo(currentController);
 
@@ -204,7 +204,7 @@ new ScrollMagic.Scene({triggerElement: "#people", duration: peopleHeight, trigge
           .setPin("#people > .enviro-bg")
           .addTo(currentController);
 
-new ScrollMagic.Scene({triggerElement: "#people-content", duration: (0.7*peopleHeight), triggerHook:0.5})
+new ScrollMagic.Scene({triggerElement: "#people-content", duration: (0.8*peopleHeight), triggerHook:0.7})
           .setClassToggle(".enviro-bg", "darken")
           .addTo(currentController);
 
