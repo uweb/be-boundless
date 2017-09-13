5b28e8de970bd9e1acc3c33291cdf8295e6b02ed
Thomas Winston Thorpe <twthorpe@uw.edu>
2017-09-12 19:06:44 -0700

    Trying the about page again…

--- page-about.php
+++ page-about.php
@@ -6,13 +6,7 @@
 ?>
 <main>
 	<?php
-		// Start the Loop.
 		while ( have_posts() ) : the_post();
-		/*
-		* Include the post format-specific template for the content. If you want to
-		* use this in a child theme, then include a file called called content-___.php
-		* (where ___ is the post format) and that will be used instead.
-		*/
 		the_content();
 		endwhile;
 	?>
