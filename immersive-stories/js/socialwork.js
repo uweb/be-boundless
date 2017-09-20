a12f19303977953fb84d381cdc04bc6c7df182ef
Thomas Winston Thorpe <twthorpe@gmail.com>
2017-09-19 23:24:18 -0700

    Social Work
    Bringing in the new text, updating some images, and finalizing the animation.

--- immersive-stories/js/socialwork.js
+++ immersive-stories/js/socialwork.js
@@ -14,19 +14,72 @@ $(function(){
         introSW.update(true);
     });
 
-	var photo2Tween = new TimelineMax()
-		.add([
-			TweenMax.to('#photo-2',1,{opacity:0,ease:Power0.easeIn,delay:0.0}),
-	])
-	var photo2Scene = new ScrollMagic.Scene({
-		triggerElement:'.transSection',
-		triggerHook:0,
-		duration:'100%',
-		reverse: true,
+	// var fade = new TimelineMax()
+	// 	.add([
+	// 		// TweenMax.from('#slide-1',1,{opacity:0,ease:Power0.easeIn}),
+	// 		TweenMax.to('#slide-2',1,{autoAlpha:1}),
+	// 		TweenMax.to('#slide-3',1,{autoAlpha:1}),
+	// 		TweenMax.to('#slide-4',1,{autoAlpha:1}),
+	// 		// TweenMax.staggerTo(['#slide-2','#slide-3','#slide-4'], 5000, {opacity:1,delay:500})
+	// 	])
+		
+	// var photo2Scene = new ScrollMagic.Scene({
+	// 	triggerElement:'.slides',
+	// 	triggerHook:0,
+	// 	offset:100,
+	// 	duration:400,
+	// 	reverse: true,
+	// })
+	// .setPin('.slides')
+	// .setTween(fade)
+	// .addIndicators()
+	// .addTo(controllerSW);
+
+
+
+	var ani2 = TweenMax.to('#slide-2', 1, {autoAlpha: 1});
+	var ani3 = TweenMax.to('#slide-3', 1, {autoAlpha: 1});
+	var ani4 = TweenMax.to('#slide-4', 1, {autoAlpha: 1});
+	
+	// scene used to pin the container
+	var pinScene = new ScrollMagic.Scene({
+		triggerHook: 0,
+		triggerElement: '.slides',
+		duration:2000
 	})
-	.setPin('.transSection')
-	.setTween(photo2Tween)
-	.addIndicators()
-	.addTo(controllerSW);
+		.setPin('.slides')
+		.addTo(controllerSW)
+		.addIndicators({zindex: 1, suffix: 'pin'});
+	
+	// scene used to fade the images
+	var slide2 = new ScrollMagic.Scene({
+		triggerHook: 0,
+		triggerElement: '.slides',
+		offset: 500,
+		duration: 100
+	})
+	.setTween(ani2)
+	.addTo(controllerSW)
+	.addIndicators({zindex: 1, suffix: 'ani'});
+
+	var slide3 = new ScrollMagic.Scene({
+		triggerHook: 0,
+		triggerElement: '.slides',
+		offset: 1000,
+		duration: 100
+	})
+	.setTween(ani3)
+	.addTo(controllerSW)
+	.addIndicators({zindex: 1, suffix: 'ani'});
+
+	var slide4 = new ScrollMagic.Scene({
+		triggerHook: 0,
+		triggerElement: '.slides',
+		offset: 1500,
+		duration: 100
+	})
+	.setTween(ani4)
+	.addTo(controllerSW)
+	.addIndicators({zindex: 1, suffix: 'ani'});
 
 });
