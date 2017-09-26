4a31aabb5e79c264519aad1590a58b0c6ded567c
Thomas Winston Thorpe <twthorpe@uw.edu>
2017-09-26 13:54:47 -0700

    Pharm content updates
    Adding the latest content updates.

--- page-kidney-health.php
+++ page-kidney-health.php
@@ -11,7 +11,7 @@
 		<div class="intro-body">
 			<div class="row">
 				<div class="col-sm-8 col-md-8 col-lg-8 col-md-offset-1 col-lg-offset-2" id='intro-text'>
-					<h1>A Pocket-Size Research Revolution</h1>
+					<h1>A POCKET-SIZE RESEARCH REVOLUTION</h1>
 					<p>With a device that can model a real kidney, researchers at the UW School of Pharmacy are giving new hope to people with kidney conditions — as well as astronauts who hope to explore the farthest reaches of space.</p>
 				</div><!-- .col-xs-8 .col-sm-8 .col-md-8 .col-lg-8 .col-xs-offset-1 .col-md-offset-1 .col-lg-offset-2 #intro-text -->
 			</div><!-- .row -->
@@ -30,7 +30,7 @@
 					<div class="col-md-10 col-md-offset-1 copy">
 						<p>Imagine that you live in a house on the beach.</p>
 						<p>Day to day, your view of the ocean never seems to change. But after 10 or 15 years, you look outside one morning and see the water lapping at your porch.</p>
-						<p>For many, that’s what it’s like to have kidney disease. “It slowly progresses over years, maybe even decades, but people often don’t know it until something goes wrong — until the water is at their door,” explains Catherine Yeung, Pharm.D., Ph.D., research assistant professor at the University of Washington School of Pharmacy.</p>
+						<p>For many, that’s what it’s like to have kidney disease. “It slowly progresses over years, maybe even decades, but people often don’t know it until something goes wrong — until the water is at their door,” explains Catherine Yeung, research assistant professor at the University of Washington School of Pharmacy.</p>
 						<p>The Centers for Disease Control and Prevention (CDC) estimates that while 15 percent of American adults have chronic kidney disease, most of them don’t know it. Only when the disease is in its later stages — when it’s often too late for reversal — do more severe symptoms cause people to seek help.</p>
 						<img src="<?php echo get_stylesheet_directory_uri() ?>/immersive-stories/img/pharmacy/img-infographic.png" alt="15% of adults in the U.S. have chronic kidney disease" class="img-responsive center-block" />
 						<p>An inability to track the early stages of chronic kidney disease leaves researchers like Yeung at a loss for what early biological markers might look like. There’s also much to learn about how kidney problems affect the body’s ability to eliminate medications.</p>
@@ -55,7 +55,7 @@
 					<div class="col-md-10 col-md-offset-1 copy">
 						<h2>A breakthrough in kidney research</h2>
 						<p>It’s called kidney-on-a-chip. Inside its small frame are thousands of live kidney cells that can model the functions of a human kidney. For pharmacy researchers, this device has opened up countless possibilities, as kidneys play a major role in the body’s ability to process medications.</p>
-						<p>“Kidneys are part of the ‘holy trinity’ of drug clearance,” says Edward Kelly, Ph.D., associate professor at the School of Pharmacy and lead investigator on the project. “The intestine is involved in absorption, the liver is involved in metabolism and the kidneys are involved in excretion,” he says, meaning that they help the body clear medication via urine.</p>
+						<p>“Kidneys are part of the ‘holy trinity’ of drug clearance,” says Edward Kelly, associate professor at the School of Pharmacy and lead investigator on the project. “The intestine is involved in absorption, the liver is involved in metabolism and the kidneys are involved in excretion,” he says, meaning that they help the body clear medication via urine.</p>
 						<p>People with failing kidneys might not be able to process medications as efficiently as those with healthy kidneys. On top of that, there’s the possibility that certain medications could further compromise unhealthy kidneys. This is critical because — unlike the liver and other organs — the kidney lacks the ability to regenerate.</p>
 						<div class="center-block">
 							<img src="<?php echo get_stylesheet_directory_uri() ?>/immersive-stories/img/pharmacy/img-scientistatdesk.jpg" alt="Team member Jonathan Himmelfarb, M.D., holds an early prototype of kidney-on-a-chip." class="img-responsive center-block" />
@@ -73,12 +73,13 @@
 					</div><!-- .col-md-3 .col-md-offset-1 .two-column-copy-left -->
 					<div class="col-md-7 two-column-copy-right">
 						<p>That’s where the UW is focusing with kidney-on-a-chip.</p>
-						<p>Working alongside Kelly and Yeung are Kenneth Thummel, Ph.D., the Milo Gibaldi endowed chair of the UW’s Department of Pharmaceutics, and Jonathan Himmelfarb, M.D., the Joseph W. Eschbach endowed chair in kidney research at the UW School of Medicine and the internationally renowned director of the Kidney Research Institute, a collaborative effort between UW Medicine and Northwest Kidney Centers. With support from both the UW and Northwest Kidney Centers, team members brought their expertise together for the kidney-on-a-chip project.</p>
+						<p>Working alongside Kelly and Yeung is Jonathan Himmelfarb, M.D., the Joseph W. Eschbach, M.D., Endowed Chair in Kidney Research at the UW School of Medicine and the internationally renowned director of the Kidney Research Institute, a collaborative effort between UW Medicine and Northwest Kidney Centers.</p>
 						<blockquote>
 							<p>“This big advance allows us to study the kidney in a very native, natural environment.”</p>
 							<cite>&mdash; Catherine Yeung, Research Assistant Professor, UW School of Pharmacy</cite>
 						</blockquote>
-						<p>The team was also instrumental in the design of the chip. They worked with UW spin-out Nortis to create a device that allows researchers to run medications through the system. Previously, researchers would put kidney cells on a plate, add medications and examine the interaction. “But that’s not what happens in the body,” says Yeung. “In the body, there’s always blood flowing, and one of the things that we’ve been able to address with kidney-on-a-chip is that we have flow.” As a result, the chip functions as a kidney normally would. “We push media through the chip using a syringe, and it flows out,” says Kelly. The fluid is then collected and tested.</p>
+						<p>With support from both the UW and Northwest Kidney Centers, team members brought their expertise together for the kidney-on-a-chip project. The team was also instrumental in the design of the chip: They worked with UW spin-out Nortis to create a device that allows researchers to run medications through the system.</p>
+						<p>Previously, researchers would put kidney cells on a plate, add medications and examine the interaction. “But that’s not what happens in the body,” says Yeung. “In the body, there’s always blood flowing, and one of the things that we’ve been able to address with kidney-on-a-chip is that we have flow.” As a result, the chip functions as a kidney normally would. “We push media through the chip using a syringe, and it flows out,” says Kelly. The fluid is then collected and tested.</p>
 						<p>With a closed system, drugs can flow through the chip without requiring gravity to keep the fluids in contact with the cells. This distinguishing factor helped the UW team be selected for special research that they’ll conduct in a place where liquids don’t follow the laws of gravity — but more on that later.</p>
 					</div><!-- .col-md-7 .two-column-copy-right -->
 				</div><!-- .row -->
@@ -158,12 +159,12 @@
 						<h2 class="h3">Kidney-on-a-chip heads to outer space</h2>
 						<p>What if you could speed up the development of kidney disease? What if you could see the water creeping toward your front door inch by inch?</p>
 						<p>In 2018, the UW team will have the opportunity to find out. They’re sending kidney-on-a-chip to the one place where time does speed up: outer space.</p>
-						<p>Microgravity acts as an accelerant, so kidney problems that take decades to develop on Earth need only weeks or months on the International Space Station (ISS), where astronauts will study the chips over the course of a few weeks.</p>
+						<p>Microgravity acts as an accelerant, so kidney problems that take decades to develop on Earth need only weeks or months on the International Space Station, where astronauts will study the chips over the course of a few weeks.</p>
 						<p>“Use of the kidney-on-a chip here on Earth has already taught us a lot about kidney function and kidney diseases,” says Himmelfarb. “The opportunity to study how the loss of gravitational forces affects kidney cellular function has the potential to improve the health of people on Earth, as well as prevent medical complications that astronauts experience from weightlessness.”</p>
-						<p>Thummel brings to the project his experience in keeping astronauts healthy. With a long-standing research interest in vitamin D metabolism and the regulation of mineral homeostasis, a key component of kidney function, Thummel was previously part of a special National Research Council committee that established guidelines for chemical exposure in spacecraft air and water.</p>
+						<p>Team member Kenneth Thummel, the Milo Gibaldi Endowed Chair in Pharmaceutics at the School of Pharmacy, has previous experience keeping astronauts healthy. With long-standing research interests in vitamin D metabolism and the regulation of mineral homeostasis, a key component of kidney function, Thummel was part of a special National Research Council committee that established guidelines for chemical exposure in spacecraft air and water.</p>
 						<p>Bone loss is also a major concern for astronauts, Kelly says. “That’s why astronauts exercise and employ other means to counter this issue when they’re in space. Kidneys synthesize the active form of vitamin D, which is necessary to maintain healthy bones, so we’ll be asking if kidney cells still perform that function in microgravity.”</p>
-						<p>Sending kidney-on-a-chip to the ISS also stands to benefit another group of space travelers: the first Mars colonists.</p>
-						<p>The current record for the most consecutive days in space is 438 — but in theory, Mars colonists would remain for decades. After studying chips on the ISS, researchers might have a better idea of how to help the human body withstand the effects of spending so much time in a lower-gravity environment.</p>
+						<p>Sending kidney-on-a-chip to the Space Station also stands to benefit another group of interstellar travelers: the first Mars colonists.</p>
+						<p>The current record for the most consecutive days in space is 438 — but in theory, Mars colonists would remain for decades. After studying chips on the Space Station, researchers might have a better idea of how to help the human body withstand the effects of spending so much time in a lower-gravity environment.</p>
 						<p>“Sending cells to space like this has never been done before,” says Yeung. “This is experimental in the truest sense of the word, which is incredibly exciting.”</p>
 						<blockquote>
 							<p>“Kidneys synthesize the active form of vitamin D, which is necessary to maintain healthy bones, so we’ll be asking if kidney cells still perform that function in microgravity.”</p>
@@ -172,8 +173,8 @@
 						<h2 class="h3 extraspace">Real-life impact for people with kidney disease</h2 class="h3">
 						<p>After the chips return from space, the UW team plans to use their improved understanding of kidney mechanics to test whether kidney-on-a-chip can be used to cure diseases, in addition to treating and preventing them.</p>
 						<p>Instead of giving patients experimental treatments that may or may not work, “We could see the effects in kidney-on-a-chip before we test them in humans,” says Kelly.</p>
-						<p>For the researchers involved, kidney-on-a-chip might very well bring about changes in their lifetime, a rarity for big medical breakthroughs. This trailblazing science is made possible in large part byharmaceutics Fund for Excellence, which invests in startup research projects like kidney-on-a-chip.</p>
-						<p>A mission to improve the lives of people around the world — and the solar system — is shared across the UW as part of the <a href="http://www.washington.edu/populationhealth">Population Health Initiative</a>. By leveraging capabilities and opportunities on and beyond, researchers will have the tools to create healthier and more fulfilling lives for all.</p>
+						<p>For the researchers involved, kidney-on-a-chip might very well bring about changes in their lifetime, a rarity for big medical breakthroughs. This trailblazing science is made possible in large part by donors who support the Pharmaceutics Fund for Excellence, which invests in startup research projects like kidney-on-a-chip.</p>
+						<p>A mission to improve the lives of people around the world — and the solar system — is shared across the UW as part of the <a href="http://www.washington.edu/populationhealth">Population Health Initiative</a>. By leveraging capabilities and opportunities on campus and beyond, researchers will have the tools to create healthier and more fulfilling lives for all.</p>
 						<p>“As a pharmacist, I’m very satisfied to be able to show people what we’re doing and where their research dollars are going,” says Yeung. “With kidney-on-a-chip, we have an actual chance to impact the lives of patients.”</p>
 						<div class="giving-widget">
 							<h2>What you care about can change the world</h2>
