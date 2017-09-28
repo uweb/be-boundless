5850d2baaf623ee649613bd32cb1049e0ed14691
Thomas Winston Thorpe <twthorpe@uw.edu>
2017-09-27 22:10:33 -0700

    Pharm bug fixes

--- page-kidney-health.php
+++ page-kidney-health.php
@@ -12,7 +12,7 @@
 			<div class="row">
 				<div class="col-sm-8 col-md-8 col-lg-8 col-md-offset-1 col-lg-offset-2" id='intro-text'>
 					<h1>A POCKET-SIZE RESEARCH REVOLUTION</h1>
-					<p>With a device that can model a real kidney, researchers at the UW School of Pharmacy are giving new hope to people with kidney conditions — as well as astronauts who hope to explore the farthest reaches of space.</p>
+					<p>With a device that can model a real kidney, researchers at the UW School of Pharmacy are giving new hope to people with kidney conditions — as well as astronauts who dream of exploring the farthest reaches of space.</p>
 				</div><!-- .col-xs-8 .col-sm-8 .col-md-8 .col-lg-8 .col-xs-offset-1 .col-md-offset-1 .col-lg-offset-2 #intro-text -->
 			</div><!-- .row -->
 		</div><!-- .intro-body -->
@@ -31,9 +31,9 @@
 						<p>Imagine that you live in a house on the beach.</p>
 						<p>Day to day, your view of the ocean never seems to change. But after 10 or 15 years, you look outside one morning and see the water lapping at your porch.</p>
 						<p>For many, that’s what it’s like to have kidney disease. “It slowly progresses over years, maybe even decades, but people often don’t know it until something goes wrong — until the water is at their door,” explains Catherine Yeung, research assistant professor at the University of Washington School of Pharmacy.</p>
+						<img src="<?php echo get_stylesheet_directory_uri() ?>/immersive-stories/img/pharmacy/img-infographic.png" alt="15% of adults in the U.S. have chronic kidney disease" class="img-responsive pull-right" />
 						<p>The Centers for Disease Control and Prevention (CDC) estimates that while 15 percent of American adults have chronic kidney disease, most of them don’t know it. Only when the disease is in its later stages — when it’s often too late for reversal — do more severe symptoms cause people to seek help.</p>
-						<img src="<?php echo get_stylesheet_directory_uri() ?>/immersive-stories/img/pharmacy/img-infographic.png" alt="15% of adults in the U.S. have chronic kidney disease" class="img-responsive center-block" />
-						<p>An inability to track the early stages of chronic kidney disease leaves researchers like Yeung at a loss for what early biological markers might look like. There’s also much to learn about how kidney problems affect the body’s ability to eliminate medications.</p>
+						<p>An inability to track the early stages of chronic kidney disease leaves researchers like Yeung at a loss to understand what early biological markers might look like. There’s also much to learn about how kidney problems affect the body’s ability to eliminate medications.</p>
 						<p>At the School of Pharmacy, an interdisciplinary team is undertaking revolutionary work that could change the future of precision medicine and kidney research — all with a device the size of a credit card.</p>
 					</div><!-- .col-md-10 .col-md-offset-1 .copy -->
 				</div><!-- .row -->
@@ -42,10 +42,10 @@
 		<section id="video" class="text-block">
 			<div class="container">
 				<div class="row">
-					<div class="col-md-10 col-md-offset-1 copy">
-						<!--<iframe width="1280" height="720" src="https://www.youtube.com/embed/9CTe-yBWu_Y?rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>-->
+					<div class="col-md-10 col-md-offset-1">
+						<iframe width="1280" height="720" src="https://www.youtube.com/embed/CYBrpCUkdVQ?rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
 						<span></span>
-					</div><!-- .col-md-10 .col-md-offset-1 .copy -->
+					</div><!-- .col-md-10 .col-md-offset-1 -->
 				</div><!-- .row -->
 			</div><!-- .container -->
 		</section><!-- #video .text-block .white -->
@@ -58,7 +58,7 @@
 						<p>“Kidneys are part of the ‘holy trinity’ of drug clearance,” says Edward Kelly, associate professor at the School of Pharmacy and lead investigator on the project. “The intestine is involved in absorption, the liver is involved in metabolism and the kidneys are involved in excretion,” he says, meaning that they help the body clear medication via urine.</p>
 						<p>People with failing kidneys might not be able to process medications as efficiently as those with healthy kidneys. On top of that, there’s the possibility that certain medications could further compromise unhealthy kidneys. This is critical because — unlike the liver and other organs — the kidney lacks the ability to regenerate.</p>
 						<div class="center-block">
-							<img src="<?php echo get_stylesheet_directory_uri() ?>/immersive-stories/img/pharmacy/img-scientistatdesk.jpg" alt="Team member Jonathan Himmelfarb, M.D., holds an early prototype of kidney-on-a-chip." class="img-responsive center-block" />
+							<img src="<?php echo get_stylesheet_directory_uri() ?>/immersive-stories/img/pharmacy/img-scientistatdesk.jpg" alt="Team member Jonathan Himmelfarb, M.D., holds an early prototype of kidney-on-a-chip." class="img-responsive center-block img-teammate" />
 							<p class="caption">Team member Jonathan Himmelfarb, M.D., holds an early prototype of kidney-on-a-chip.</p>
 						</div><!-- .center-block -->
 					</div><!-- .col-md-10 .col-md-offset-1 .copy -->
@@ -68,10 +68,8 @@
 		<section class="text-block graphpaper-white first">
 			<div class="container">
 				<div class="row">
-					<div class="col-md-3 col-md-offset-1 two-column-copy-left">
-						<img src="<?php echo get_stylesheet_directory_uri() ?>/immersive-stories/img/pharmacy/img-chip.png" alt="Photograph of the Kidney on a Chip" class="img-responsive center-block" />
-					</div><!-- .col-md-3 .col-md-offset-1 .two-column-copy-left -->
-					<div class="col-xs-12 col-sm-10 col-sm-offset-1 col-md-7 two-column-copy-right">
+					<div class="col-md-10 col-md-offset-1 copy">
+						<img src="<?php echo get_stylesheet_directory_uri() ?>/immersive-stories/img/pharmacy/img-chip.png" alt="Photograph of the Kidney on a Chip" class="img-responsive pull-left img-chip" />
 						<p>That’s where the UW is focusing with kidney-on-a-chip.</p>
 						<p>Working alongside Kelly and Yeung is Jonathan Himmelfarb, M.D., the Joseph W. Eschbach, M.D., Endowed Chair in Kidney Research at the UW School of Medicine and the internationally renowned director of the Kidney Research Institute, a collaborative effort between UW Medicine and Northwest Kidney Centers.</p>
 						<blockquote>
@@ -81,27 +79,17 @@
 						<p>With support from both the UW and Northwest Kidney Centers, team members brought their expertise together for the kidney-on-a-chip project. The team was also instrumental in the design of the chip: They worked with UW spin-out Nortis to create a device that allows researchers to run medications through the system.</p>
 						<p>Previously, researchers would put kidney cells on a plate, add medications and examine the interaction. “But that’s not what happens in the body,” says Yeung. “In the body, there’s always blood flowing, and one of the things that we’ve been able to address with kidney-on-a-chip is that we have flow.” As a result, the chip functions as a kidney normally would. “We push media through the chip using a syringe, and it flows out,” says Kelly. The fluid is then collected and tested.</p>
 						<p>With a closed system, drugs can flow through the chip without requiring gravity to keep the fluids in contact with the cells. This distinguishing factor helped the UW team be selected for special research that they’ll conduct in a place where liquids don’t follow the laws of gravity — but more on that later.</p>
-					</div><!-- .col-md-7 .two-column-copy-right -->
+					</div><!-- .col-md-10 .col-md-offset-1 .copy -->
 				</div><!-- .row -->
 			</div><!-- .container -->
 		</section><!-- .text-block .graphpaper-white .first -->
-		<section class="text-block white">
-			<div class="container">
-				<div class="row">
-					<div class="col-md-10 col-md-offset-1 copy lessspaceforjj">
-						<h2 class="h3">A faster, safer method for testing medications</h2>
-						<p>By giving researchers the ability to test multiple drugs at the same time, the chip may lead to a quicker and safer approval process for new medications. Because the harmful effects of medications would be detected with the chip, the need for human and animal test subjects would drop dramatically.</p>
-					</div><!-- .col-md-10 .col-md-offset-1 .copy .lessspaceforjj -->
-				</div><!-- .row -->
-			</div><!-- .container -->
-		</section><!-- .text-block .white -->
 		<section class="text-block giving-widget-promoted">
 			<div class="container">
 				<div class="row">
 					<div class="col-xs-12 col-sm-6 col-sm-offset-6 col-md-4 col-md-offset-5 col-lg-6 col-lg-offset-4">
 						<div>
 							<h2>A brighter future for patients everywhere</h2>
-							<p>Kidney-on-a-chip is one of many School of Pharmacy endeavors that aim to improve disease treatment and prevention through precision medicine. By supporting the Pharmaceutics Fund for Excellence, you can help develop solutions for the world’s biggest medical challenges.</p>
+							<p>Kidney-on-a-chip is one of the UW’s many efforts to tackle today’s biggest medical challenges. By supporting the Pharmaceutics Fund for Excellence, you can help improve disease treatment and prevention around the world.</p>
 						</div>
 					</div><!-- .first-col .col-xs-12 .col-sm-6 .col-sm-offset-6 .col-md-4 .col-md-offset-4 -->
 					<div class="col-sec col-md-3 col-lg-2 visible-md-block visible-lg-block">
@@ -119,6 +107,16 @@
 			</div><!-- .container -->
 			<div id="immersive-give-iframe-promoted"></div>
 		</section>
+		<section class="text-block white">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-10 col-md-offset-1 copy lessspaceforjj">
+						<h2 class="h3">A faster, safer method for testing medications</h2>
+						<p>By giving researchers the ability to test multiple drugs at the same time, the chip may lead to a quicker and safer approval process for new medications. Because the harmful effects of medications would be detected with the chip, the need for human and animal test subjects would drop dramatically.</p>
+					</div><!-- .col-md-10 .col-md-offset-1 .copy .lessspaceforjj -->
+				</div><!-- .row -->
+			</div><!-- .container -->
+		</section><!-- .text-block .white -->
 		<section class="text-block graphpaper-white second video1">
 			<div class="container">
 				<div class="row">
