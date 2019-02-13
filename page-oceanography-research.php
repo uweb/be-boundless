cb3c2536fca6e9134c2fba186358d16f5339e8ca
Teri Shelton <tjshelt@uw.edu>
2019-02-13 09:32:44 -0800

    minor update to RV Carson v2 mainly swapping the animation placeholder and the microscope video

--- page-oceanography-research.php
+++ page-oceanography-research.php
@@ -101,22 +101,23 @@
 
                                 <div class="photo-caption overlap-photo image1">
                                     <img src="<?php echo get_stylesheet_directory_uri() .'/immersive-stories/img/rv-carson/deana-crouser.jpg' ?>" alt="Oceanography major Deana Crouser, '19." />
-                                    <p class="caption">Oceanography major Deana Crouser, '19</p>
+                                    <p class="caption">Oceanography major Deana Crouser, &rsquo;19</p>
                                 </div>
 
-                                <blockquote class="quote1">It felt like I was in a TED Talk every time I went to class, the lab was very hands-on. You have to do lots of experiments, and then you go on a day trip on the RV Carson. Once I did that, I was all in. <cite>&mdash; Deana Crouser</cite></blockquote>
+                                <blockquote class="quote1">It felt like I was in a TED Talk every time I went to class. You have to do lots of experiments, and then you go on a day trip on the RV Carson. Once I did that, I was all in. <cite>&mdash; Deana Crouser</cite></blockquote>
 
-                                <div class="photo-caption video">
-                                    <a class="zooplankton-video" data-lity href="https://www.youtube.com/embed/J5YOMEcYpUg?autoplay=1&rel=0&showinfo=0">
+                                <div class="photo-caption animation">
+                                    <a class="zooplankton-animation" data-lity href="https://www.youtube.com/embed/I_GvxsadWmM?autoplay=1&rel=0&showinfo=0">
                                         <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-                                         width="50px" height="50px" viewBox="0 0 137.909 137.131" enable-background="new 0 0 137.909 137.131"
+                                         width="137.909px" height="137.131px" viewBox="0 0 137.909 137.131" enable-background="new 0 0 137.909 137.131"
                                          xml:space="preserve">
                                             <ellipse fill="none" stroke="#FFFFFF" stroke-width="7.8281" stroke-miterlimit="10" cx="68.399" cy="68.344" rx="57.675" ry="59.214"/>
                                             <polygon fill="#FFFFFF" points="48.611,37.617 103.268,68.345 48.611,99.072 "/>
                                         </svg>
                                     </a>
-                                    <p class="caption"><a data-lity href="https://www.youtube.com/embed/J5YOMEcYpUg?autoplay=1&rel=0&showinfo=0">Play full video</a></p>
+                                    <p class="caption">ANIMATION PLACEHOLDER</p>
                                 </div>
+
                             </div><!-- .row .grid -->
                         </div><!-- .col-md-10 .col-md-offset-1 .copy -->
                     </div><!-- .row -->
@@ -185,16 +186,16 @@
                                     <p class="caption">Professor Daniel Gr&#252;nbaum assembles infrared cameras to film zooplankton in dark waters.</p>
                                 </div>
 
-                                <div class="photo-caption animation">
-                                    <a class="zooplankton-animation" data-lity href="https://www.youtube.com/embed/I_GvxsadWmM?autoplay=1&rel=0&showinfo=0">
+                                <div class="photo-caption video">
+                                    <a class="zooplankton-video" data-lity href="https://www.youtube.com/embed/J5YOMEcYpUg?autoplay=1&rel=0&showinfo=0">
                                         <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-                                         width="137.909px" height="137.131px" viewBox="0 0 137.909 137.131" enable-background="new 0 0 137.909 137.131"
+                                         width="50px" height="50px" viewBox="0 0 137.909 137.131" enable-background="new 0 0 137.909 137.131"
                                          xml:space="preserve">
                                             <ellipse fill="none" stroke="#FFFFFF" stroke-width="7.8281" stroke-miterlimit="10" cx="68.399" cy="68.344" rx="57.675" ry="59.214"/>
                                             <polygon fill="#FFFFFF" points="48.611,37.617 103.268,68.345 48.611,99.072 "/>
                                         </svg>
                                     </a>
-                                    <p class="caption">ANIMATION PLACEHOLDER</p>
+                                    <p class="caption"><a data-lity href="https://www.youtube.com/embed/J5YOMEcYpUg?autoplay=1&rel=0&showinfo=0">Play full video</a></p>
                                 </div>
                             </div><!-- .row .grid -->
                         </div><!-- .col-md-10 .col-md-offset-1 .copy -->
