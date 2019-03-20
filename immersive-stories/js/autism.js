849db60982bd91977b6ff7e6df8c98bc5c94aada
Teri Shelton <tjshelt@uw.edu>
2019-03-20 14:10:08 -0700

    Adjusted the rollover effect for Autism v1

--- immersive-stories/js/autism.js
+++ immersive-stories/js/autism.js
@@ -41,12 +41,12 @@ $(function () {
         e.preventDefault();
     });
 
-    // header "parallax" effect
+    // header "scroll" effect
     //var $el = $('.intro-parallax');
     //$(window).on('scroll', function () {
     //    var scroll = $(document).scrollTop();
     //    $el.css({
-    //        'background-position':'50% '+(-0.9*scroll)+'px'
+    //        'background-position':'50% '+(-0.5*scroll)+'px'
     //    });
     //});
 
@@ -59,11 +59,11 @@ $(function () {
         var coorX = ( halfW - ( event.pageX - this.offsetLeft ) );
         var coorY = ( halfH - ( event.pageY - $(this).offset().top ) ); // this is the problem
 
-        var degX  = ( ( coorY / halfH ) * 5 ) + 'deg'; // max. degree = 10 /// this is the problem
-        var degY  = ( ( coorX / halfW ) * -5 ) + 'deg'; // max. degree = 10
+        var degX  = ( ( coorY / halfH ) * 10 ) + 'deg'; // max. degree = 10 /// this is the problem
+        var degY  = ( ( coorX / halfW ) * -10 ) + 'deg'; // max. degree = 10
 
         $( this ).css( 'transform', function() {
-          return 'perspective( 1500px ) translate3d( 0, 0, 30px )  rotateX('+ degX +') rotateY('+ degY +')';
+          return 'perspective( 600px ) translate3d( 0, 0, 30px )  rotateX('+ degX +') rotateY('+ degY +')';
         } )
 
       } )
