1fd42e86c7653cb3e6ce5d72d24adda6d8fa41d6
Lauren V Caple <lcaple@uw.edu>
2019-09-20 11:58:55 -0700

    added additional domains to GA tracking

--- footer-campaign-v2.php
+++ footer-campaign-v2.php
@@ -72,7 +72,9 @@ global $scripts;
 				(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 				m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 				})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-				ga('create', 'UA-15747328-1', 'auto');
+				ga('create', 'UA-15747328-1', 'auto', {'allowLinker': true});
+				ga('require', 'linker');
+				ga('linker:autoLink', ['online.gifts.washington.edu'], ['gifts.washington.edu']);
 				ga('send', 'pageview');
 			</script>
 			<!-- Global site tag (gtag.js) - Google Analytics -->
