3b1a3cf5e4497adb833e30019c03e25d61eb0f71
Teri Shelton <tjshelt@uw.edu>
2019-12-19 12:44:51 -0800

    change image calc

--- immersive-stories/js/2019-year-in-review.js
+++ immersive-stories/js/2019-year-in-review.js
@@ -1,13 +1,13 @@
 $(function () {
     // Blur to focus on scroll images
     var blurScroll = 0;
-	var diverPhoto = $( '#flex-content-section-1' ).offset().top;
+	var diverPhoto = $( '#flex-content-section-1 img' ).offset().top;
 	console.log(diverPhoto);
-	var crewPhoto = $( '#flex-content-section-5' ).offset().top;
+	var crewPhoto = $( '#flex-content-section-5 img' ).offset().top;
 	console.log(crewPhoto);
-	var forestPhoto = $( '#flex-content-section-8' ).offset().top;
+	var forestPhoto = $( '#flex-content-section-8 img' ).offset().top;
 	console.log(forestPhoto);
-	var microplasticsPhoto = $( '#flex-content-section-16' ).offset().top;
+	var microplasticsPhoto = $( '#flex-content-section-16 img' ).offset().top;
 	console.log(microplasticsPhoto);
 
 	$( '#flex-content-section-1 img' ).addClass('blur-focus');
@@ -26,19 +26,19 @@ $(function () {
         }
 
         // crew photo
-        if ( blurScroll > ( crewPhoto + 1500 ) ) {
+        if ( blurScroll > ( crewPhoto + 1000 ) ) {
 			$('#flex-content-section-5 img').removeClass('blur-focus');
 			$('#flex-content-section-5 img').addClass('focus-blur');
         }
 
         // forest photo
-        if ( blurScroll > ( forestPhoto + 2000 ) ) {
+        if ( blurScroll > ( forestPhoto + 2500 ) ) {
             $('#flex-content-section-8 img').removeClass('blur-focus');
             $('#flex-content-section-8 img').addClass('focus-blur');
         }
 
 		// microplastics photo
-		if ( blurScroll > ( microplasticsPhoto * 1.5 ) ) {
+		if ( blurScroll > ( microplasticsPhoto * 1.8 ) ) {
             $('#flex-content-section-16 img').removeClass('blur-focus');
             $('#flex-content-section-16 img').addClass('focus-blur');
         }
