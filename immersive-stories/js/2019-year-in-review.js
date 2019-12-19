331bbe7fd9eb4f5b225d70b89afd578c6a775eac
Teri Shelton <tjshelt@uw.edu>
2019-12-19 13:05:43 -0800

    commented code and removed from story

--- immersive-stories/js/2019-year-in-review.js
+++ immersive-stories/js/2019-year-in-review.js
@@ -1,46 +1,46 @@
-$(function () {
-    // Blur to focus on scroll images
-    var blurScroll = 0;
-	var diverPhoto = $( '#flex-content-section-1 img' ).offset().top;
-	console.log(diverPhoto);
-	var crewPhoto = $( '#flex-content-section-5 img' ).offset().top;
-	console.log(crewPhoto);
-	var forestPhoto = $( '#flex-content-section-8 img' ).offset().top;
-	console.log(forestPhoto);
-	var microplasticsPhoto = $( '#flex-content-section-16 img' ).offset().top;
-	console.log(microplasticsPhoto);
+// $(function () {
+//     // Blur to focus on scroll images
+//     var blurScroll = 0;
+// 	var diverPhoto = $( '#flex-content-section-1 img' ).offset().top;
+// 	console.log(diverPhoto);
+// 	var crewPhoto = $( '#flex-content-section-5 img' ).offset().top;
+// 	console.log(crewPhoto);
+// 	var forestPhoto = $( '#flex-content-section-8 img' ).offset().top;
+// 	console.log(forestPhoto);
+// 	var microplasticsPhoto = $( '#flex-content-section-16 img' ).offset().top;
+// 	console.log(microplasticsPhoto);
 
-	$( '#flex-content-section-1 img' ).addClass('blur-focus');
-	$( '#flex-content-section-5 img' ).addClass('blur-focus');
-	$( '#flex-content-section-8 img' ).addClass('blur-focus');
-	$( '#flex-content-section-16 img' ).addClass('blur-focus');
+// 	$( '#flex-content-section-1 img' ).addClass('blur-focus');
+// 	$( '#flex-content-section-5 img' ).addClass('blur-focus');
+// 	$( '#flex-content-section-8 img' ).addClass('blur-focus');
+// 	$( '#flex-content-section-16 img' ).addClass('blur-focus');
 
-    $(window).scroll(function () {
-		blurScroll = $(window).scrollTop();
-		console.log(blurScroll);
+//     $(window).scroll(function () {
+// 		blurScroll = $(window).scrollTop();
+// 		console.log(blurScroll);
 
-        // diver photo
-        if ( blurScroll > diverPhoto ) {
-			$('#flex-content-section-1 img').removeClass('blur-focus');
-			$('#flex-content-section-1 img').addClass('focus-blur');
-        }
+//         // diver photo
+//         if ( blurScroll > diverPhoto ) {
+// 			$('#flex-content-section-1 img').removeClass('blur-focus');
+// 			$('#flex-content-section-1 img').addClass('focus-blur');
+//         }
 
-        // crew photo
-        if ( blurScroll > ( crewPhoto + 1000 ) ) {
-			$('#flex-content-section-5 img').removeClass('blur-focus');
-			$('#flex-content-section-5 img').addClass('focus-blur');
-        }
+//         // crew photo
+//         if ( blurScroll > ( crewPhoto + 1000 ) ) {
+// 			$('#flex-content-section-5 img').removeClass('blur-focus');
+// 			$('#flex-content-section-5 img').addClass('focus-blur');
+//         }
 
-        // forest photo
-        if ( blurScroll > ( forestPhoto + 2500 ) ) {
-            $('#flex-content-section-8 img').removeClass('blur-focus');
-            $('#flex-content-section-8 img').addClass('focus-blur');
-        }
+//         // forest photo
+//         if ( blurScroll > ( forestPhoto + 2500 ) ) {
+//             $('#flex-content-section-8 img').removeClass('blur-focus');
+//             $('#flex-content-section-8 img').addClass('focus-blur');
+//         }
 
-		// microplastics photo
-		if ( blurScroll > ( microplasticsPhoto * 1.9 ) ) {
-            $('#flex-content-section-16 img').removeClass('blur-focus');
-            $('#flex-content-section-16 img').addClass('focus-blur');
-        }
-    });
-});
+// 		// microplastics photo
+// 		if ( blurScroll > ( microplasticsPhoto * 1.9 ) ) {
+//             $('#flex-content-section-16 img').removeClass('blur-focus');
+//             $('#flex-content-section-16 img').addClass('focus-blur');
+//         }
+//     });
+// });
