1f64b7800b812082ee3bf0fc5b12dedc74538ff4
Teri Shelton <tjshelt@uw.edu>
2019-12-19 12:22:03 -0800

    more blur/focus

--- immersive-stories/js/2019-year-in-review.js
+++ immersive-stories/js/2019-year-in-review.js
@@ -1,10 +1,10 @@
 $(function () {
     // Blur to focus on scroll images
     var blurScroll = 0;
-    var diverPhoto = $( '#flex-content-section-1 img' ).offset().top;
-    var crewPhoto = $( '#flex-content-section-5 img' ).offset().top;
-	var forestPhoto = $( '#flex-content-section-8 img' ).offset().top;
-	var microplasticsPhoto = $( '#flex-content-section-16 img' ).offset().top;
+    var diverPhoto = $( '#flex-content-section-1' ).offset().top;
+    var crewPhoto = $( '#flex-content-section-5' ).offset().top;
+	var forestPhoto = $( '#flex-content-section-8' ).offset().top;
+	var microplasticsPhoto = $( '#flex-content-section-16' ).offset().top;
 
 	$( '#flex-content-section-1 img' ).addClass('blur-focus');
 	$( '#flex-content-section-5 img' ).addClass('blur-focus');
@@ -15,25 +15,25 @@ $(function () {
         blurScroll = $(window).scrollTop();
 
         // diver photo
-        if (blurScroll > diverPhoto ) {
+        if (blurScroll > diverPhoto + 50 ) {
 			$('#flex-content-section-1 img').removeClass('blur-focus');
 			$('#flex-content-section-1 img').addClass('focus-blur');
         }
 
         // crew photo
-        if (blurScroll > crewPhoto ) {
+        if (blurScroll > crewPhoto + 50 ) {
 			$('#flex-content-section-5 img').removeClass('blur-focus');
 			$('#flex-content-section-5 img').addClass('focus-blur');
         }
 
         // forest photo
-        if (blurScroll > forestPhoto ) {
+        if (blurScroll > forestPhoto + 50 ) {
             $('#flex-content-section-8 img').removeClass('blur-focus');
             $('#flex-content-section-8 img').addClass('focus-blur');
         }
 
 		// microplastics photo
-		if (blurScroll > microplasticsPhoto ) {
+		if (blurScroll > microplasticsPhoto + 50 ) {
             $('#flex-content-section-16 img').removeClass('blur-focus');
             $('#flex-content-section-16 img').addClass('focus-blur');
         }
