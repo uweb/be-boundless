1ab00ff4dd4542bb8b9950b980001e7a3e701ca2
Lauren V Caple <lcaple@uw.edu>
2020-03-19 13:03:47 -0700

    switched default funds to include those with COVID-19 messaging

--- header-campaign-v2.php
+++ header-campaign-v2.php
@@ -4,7 +4,11 @@ global $styles;
 global $storyCodes;
 global $storyAppealCodes;
 if(!$storyCodes || $storyCodes == ''){
-	$storyCodes = 'HEROES,SEALTS,KATRIN,UWTCHA,UWBSEF,IMPFND';
+	//Emergency relief funds - non-COVID-19 messaging
+	// $storyCodes = 'HEROES,SEALTS,KATRIN,UWTCHA,UWBSEF,IMPFND';
+
+	//COVID-19 specific relief funds + messaging
+	$storyCodes = 'HEROES,SEALTS,SEACOV,TACCOV,BOTCOV,IMPFND';
 }
 if(!$storyAppealCodes || $storyAppealCodes == ''){
 	$storyAppealCodes = '20XBS';
