c1237c427b506e4f1cf4d698328e4b2a20c3b2b6
Teri Shelton <tjshelt@uw.edu>
2020-04-03 13:21:02 -0700

    removed comments and tweaked JS file for image swap

--- immersive-stories/js/native-languages.js
+++ immersive-stories/js/native-languages.js
@@ -2,7 +2,7 @@ $(function () {
 	// Image swap on scroll
 	var swapScrollNum = 0;
 	var displayPanel = $('#flex-content-section-6').offset().top;
-	$( '#flex-content-section-7' ).addClass( 'lushootseed-writing-2' );
+	$( '#flex-content-section-6' ).addClass( 'lushootseed-writing-2' );
 	$(window).scroll(function () {
 		swapScrollNum = $(window).scrollTop();
 		if ( swapScrollNum > ( displayPanel - $( '#flex-content-section-6' ).height() / 2 ) && ( swapScrollNum < ( displayPanel + $( '#flex-content-section-7' ).height() / 1.5 )) ) {
