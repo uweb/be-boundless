6584bdcf091c7292e40d1f6f15512a287f8303a7
Teri Shelton <tjshelt@uw.edu>
2020-04-03 17:47:24 -0700

    another fix to the container ID

--- immersive-stories/js/native-languages.js
+++ immersive-stories/js/native-languages.js
@@ -5,7 +5,7 @@ $(function () {
 	$( '#flex-content-section-6' ).addClass( 'lushootseed-writing-2' );
 	$(window).scroll(function () {
 		swapScrollNum = $(window).scrollTop();
-		if ( swapScrollNum > ( displayPanel - $( '#flex-content-section-6' ).height() / 2 ) && ( swapScrollNum < ( displayPanel + $( '#flex-content-section-7' ).height() / 1.5 )) ) {
+		if ( swapScrollNum > ( displayPanel - $( '#flex-content-section-6' ).height() / 2 ) && ( swapScrollNum < ( displayPanel + $( '#flex-content-section-6' ).height() / 1.5 )) ) {
 			$( '#flex-content-section-6' ).addClass( 'lushootseed-writing-2' ).removeClass( 'lushootseed-writing-1');
 		} else {
 			$( '#flex-content-section-6' ).addClass( 'lushootseed-writing-1' ).removeClass( 'lushootseed-writing-2');
