43b5175d21187ba9184ea89a37420f8458bff324
Lauren V Caple <lcaple@uw.edu>
2020-09-11 16:09:56 -0700

    added overlay option for homepage banner

--- setup/campaign/class.home-slide-shortcode.php
+++ setup/campaign/class.home-slide-shortcode.php
@@ -1,9 +1,9 @@
-<?php 
+<?php
 
 /*
  *  Boundless Image shortcode allows for styled image slide to be added to content
  *  r
- *  optional image url, assumes featured image by default 
+ *  optional image url, assumes featured image by default
  *  optional title, assumes page title by default (uses false to disable)
  *  optional array of classes to insert
  */
@@ -26,7 +26,7 @@ class Campaign_Intro
         $attributes = (object) shortcode_atts( array(
             'image' => null, //wp_get_attachment_image_src( get_post_thumbnail_id() , 'full')[0],
             'branding' => true,
-            'title' => get_the_title(), 
+            'title' => get_the_title(),
             'classes' => null,
             'video' => false
         ), $atts );
@@ -36,7 +36,7 @@ class Campaign_Intro
             return;
         }
 
-        $attributes->classes = (!$attributes->classes) ? array() : $attributes->classes;
+        $class_string = (!$attributes->classes) ? '' : $attributes->classes . ' ';
 
         $attributes->image = (!$attributes->image) ? wp_get_attachment_image_src( get_post_thumbnail_id() , 'full') : $attributes->image;
 
@@ -55,20 +55,20 @@ class Campaign_Intro
 
             $video = ''.
             '<button id="pause">Pause</button>' .
-              '<video  autoplay loop muted playsinline class="media" data-object-fit poster="' . $attributes->image . '" class="fullscreen-bg__video">' . 
-                  '<source src="' . $video_src . '" type="video/mp4">' . 
+              '<video  autoplay loop muted playsinline class="media" data-object-fit poster="' . $attributes->image . '" class="fullscreen-bg__video">' .
+                  '<source src="' . $video_src . '" type="video/mp4">' .
               '</video>';
 
             array_push($classes, 'cv2-home-slide-video');
         }
-        
+
         if (!$attributes->branding){
             //set to featured image
             array_push($classes, 'hide-branding');
         }
 
 
-        $class_string = implode($classes, ' ');
+        $class_string .= implode($classes, ' ');
 
         return sprintf('<section id="campaign-intro" class="intro %s" style="background-image: url(%s)">%s<div class="main-content">%s<div class="container">%s</div></div><img id="boundless-logo" src="%s" alt="Be boundless"><section class="scrollit read-more"><p>Scroll Down</p><div class="mouse"><div class="scroll"></div></div></section><img id="block-w-logo" src="%s" alt=""></section>', $class_string, $attributes->image, $video, $attributes->title, $content, get_stylesheet_directory_uri() . '/campaign/img/be-boundless.png', get_stylesheet_directory_uri() . '/campaign/img/W-Logo_White.png');
 
